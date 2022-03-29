8f7b8647ea09d7632fae5d7542ee6cf5c0c78ffd
Hugo Santos <hugo@namespacelabs.com>
2022-03-29 20:54:40 +0200

    internal/disk: add a comment on why we don't produce fs type in MacOS.

--- internal/disk/disk_darwin.go
+++ internal/disk/disk_darwin.go
@@ -3,8 +3,9 @@
 
 package disk
 
-import "syscall"
-
+// We don't require filesystem information in MacOS. Filesystem information
+// is used temporarily as part of #121, which doesn't affect MacOS. This
+// package is expected to be removed afterwards.
 func FSType(path string) (string, error) {
 	return "unknown", nil
 }
