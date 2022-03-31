5382a275224ae6096311253a0ffdb53f78a45f4e
Hugo Santos <hugo@namespacelabs.com>
2022-03-31 13:28:21 +0200

    sdk/k3d: relaxed the runc version parsing a bit, to support debian-style versions.
    Fixes #142.

---
+++ internal/sdk/k3d/version_test.go
@@ -0,0 +1,25 @@
+package k3d
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+)
+
+func TestDockerVersionParsing(t *testing.T) {
+	for _, test := range []struct {
+		ver types.Version
+		ok  bool
+	}{
+		{ver: types.Version{Version: "20.10.13"}, ok: false},
+		{ver: types.Version{Version: "20.10.13", Components: []types.ComponentVersion{{Name: "runc", Version: "1.0.3"}}}, ok: true},
+		{ver: types.Version{Version: "20.10.5+dfsg1", Components: []types.ComponentVersion{{Name: "runc", Version: "1.0.0~rc93+ds1"}}}, ok: true},
+	} {
+		dockerOK, runcOK, _ := validateVersions(test.ver)
+		if (dockerOK && runcOK) != test.ok {
+			serialized, _ := json.Marshal(test.ver)
+			t.Errorf("failed to parse, expected ok=%v, got=(%v && %v), ver was %s", test.ok, dockerOK, runcOK, serialized)
+		}
+	}
+}
