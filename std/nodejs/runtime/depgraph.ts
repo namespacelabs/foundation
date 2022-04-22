393d7b0aa85a532e3f7d26f35d6513fc7f907dc6
Nik Zherebtsov <nz@namespacelabs.com>
2022-04-22 17:53:36 +0200

    Fixes.

--- std/nodejs/runtime/depgraph.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Namespace Labs Inc; All rights reserved.
-// Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
-// available at http://github.com/namespacelabs/foundation
-
-export class DependencyGraph {
-	instantiate() {
-		console.log("instantiate");
-	}
-}
