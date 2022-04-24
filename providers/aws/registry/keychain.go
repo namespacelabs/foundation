5643baf454bc16bd7245c1418bccc510fd31386c
Hugo Santos <hugo@namespacelabs.com>
2022-04-24 20:52:01 +0200

    aws/registry: handle ssocreds.InvalidTokenError.

--- providers/aws/registry/keychain.go
+++ providers/aws/registry/keychain.go
@@ -6,9 +6,12 @@ package registry
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/ssocreds"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
@@ -99,6 +102,13 @@ func (r *resolveAccount) Inputs() *compute.In { return compute.Inputs().Str("pro
 func (r *resolveAccount) Compute(ctx context.Context, _ compute.Resolved) (*sts.GetCallerIdentityOutput, error) {
 	out, err := sts.NewFromConfig(r.sesh).GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
 	if err != nil {
+		var e *ssocreds.InvalidTokenError
+		if errors.As(err, &e) {
+			return nil, fnerrors.UsageError(
+				fmt.Sprintf("Try running `aws --profile %s sso login`.", r.profile),
+				"AWS session credentials have expired.")
+		}
+
 		return nil, err
 	}
 
