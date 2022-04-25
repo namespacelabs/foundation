bb6cef7456edc04ced51f0c136737fdc7626f6e3
Nikolay Zherebtsov <nz@namespacelabs.com>
2022-04-25 10:31:17 +0200

    Basic support for extensions in Node.js (#274)

--- languages/shared/api.go
+++ languages/shared/api.go
@@ -4,7 +4,10 @@
 
 package shared
 
-import "namespacelabs.dev/foundation/workspace"
+import (
+	"namespacelabs.dev/foundation/schema"
+	"namespacelabs.dev/foundation/workspace"
+)
 
 type ServerData struct {
 	Services []EmbeddedServiceData
@@ -12,4 +15,37 @@ type ServerData struct {
 
 type EmbeddedServiceData struct {
 	Location workspace.Location
+}
+
+type NodeData struct {
+	Service   *ServiceData
+	Providers []ProviderData
+}
+
+type ServiceData struct {
+	Deps []DependencyData
+}
+
+type TypeData struct {
+	Name           string
+	SourceFileName string
+	PackageName    schema.PackageName
+}
+
+type ProviderData struct {
+	Name         string
+	InputType    TypeData
+	ProviderType *schema.Provides_AvailableIn
+}
+
+type DependencyData struct {
+	Name             string
+	Provider         ProviderData
+	ProviderLocation workspace.Location
+	ProviderInput    SerializedProto
+}
+
+type SerializedProto struct {
+	Content  []byte
+	Comments []string
 }
--- languages/shared/prepare.go
+++ languages/shared/prepare.go
@@ -6,9 +6,16 @@ package shared
 
 import (
 	"context"
+	"strings"
 
+	"github.com/protocolbuffers/txtpbfmt/parser"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/dynamicpb"
+	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/schema"
 	"namespacelabs.dev/foundation/workspace"
+	"namespacelabs.dev/foundation/workspace/source/protos"
 )
 
 // Prepare codegen data for a server.
@@ -29,4 +36,121 @@ func PrepareServerData(ctx context.Context, loader workspace.Packages, loc works
 	}
 
 	return serverData, nil
+}
+
+func PrepareNodeData(ctx context.Context, loader workspace.Packages, loc workspace.Location, n *schema.Node, fmwk schema.Framework) (NodeData, error) {
+	var nodeData NodeData
+
+	if n.ExportService != nil {
+		deps := []DependencyData{}
+		for _, dep := range n.GetInstantiate() {
+			pkg, err := loader.LoadByName(ctx, schema.PackageName(dep.PackageName))
+			if err != nil {
+				return NodeData{}, fnerrors.UserError(nil, "failed to load %s/%s: %w", dep.PackageName, dep.Type, err)
+			}
+
+			_, p := workspace.FindProvider(pkg, schema.PackageName(dep.PackageName), dep.Type)
+			if p == nil {
+				return NodeData{}, fnerrors.UserError(nil, "didn't find a provider for %s/%s", dep.PackageName, dep.Type)
+			}
+
+			var provider *schema.Provides_AvailableIn
+			for _, prov := range p.AvailableIn {
+				if prov.ProvidedInFrameworks()[fmwk] {
+					provider = prov
+					break
+				}
+			}
+
+			providerInput, err := serializeProto(ctx, pkg, p, dep)
+			if err != nil {
+				return NodeData{}, err
+			}
+
+			deps = append(deps, DependencyData{
+				Name: dep.Name,
+				Provider: ProviderData{
+					Name:         p.Name,
+					InputType:    convertType(p.Type, schema.PackageName(dep.PackageName)),
+					ProviderType: provider,
+				},
+				ProviderLocation: pkg.Location,
+				ProviderInput:    *providerInput,
+			})
+		}
+
+		nodeData.Service = &ServiceData{
+			Deps: deps,
+		}
+	}
+	for _, p := range n.Provides {
+		for _, a := range p.AvailableIn {
+			if a.ProvidedInFrameworks()[fmwk] {
+				nodeData.Providers = append(nodeData.Providers, ProviderData{
+					Name:         p.Name,
+					InputType:    convertType(p.Type, schema.PackageName(n.PackageName)),
+					ProviderType: a,
+				})
+			}
+		}
+	}
+
+	return nodeData, nil
+}
+
+func convertType(t *schema.TypeDef, pkgName schema.PackageName) TypeData {
+	nameParts := strings.Split(string(t.Typename), ".")
+	// TODO(@nicolasalt): check that the sources contain at least one file.
+	return TypeData{
+		Name:           nameParts[len(nameParts)-1],
+		SourceFileName: t.Source[0],
+		PackageName:    pkgName,
+	}
+}
+
+// Copied from "languages/golang/dependency.go#serializeProto"
+func serializeProto(ctx context.Context, pkg *workspace.Package, provides *schema.Provides, instance *schema.Instantiate) (*SerializedProto, error) {
+	serializedProto := SerializedProto{
+		Comments: []string{},
+	}
+
+	parsed, ok := pkg.Provides[provides.Name]
+	if !ok {
+		return nil, fnerrors.InternalError("%s: protos were not loaded as expected?", instance.PackageName)
+	}
+
+	files, msgdesc, err := protos.LoadMessageByName(parsed, provides.Type.Typename)
+	if err != nil {
+		return nil, fnerrors.InternalError("%s: failed to load message %q: %w", instance.PackageName, provides.Type.Typename, err)
+	}
+
+	raw := dynamicpb.NewMessage(msgdesc)
+	if err := proto.Unmarshal(instance.Constructor.Value, raw.Interface()); err != nil {
+		return nil, fnerrors.InternalError("failed to unmarshal constructor: %w", err)
+	}
+
+	// Clean up all values which are not meant to be shipped into the binary.
+	protos.CleanupForNonProvisioning(raw)
+
+	deterministicBytes, err := proto.MarshalOptions{Deterministic: true}.Marshal(raw.Interface())
+	if err != nil {
+		return nil, fnerrors.InternalError("failed to marshal depvar: %w", err)
+	}
+
+	serializedProto.Content = deterministicBytes
+
+	resolver, err := protos.AsResolver(files)
+	if err != nil {
+		return nil, fnerrors.InternalError("failed to create resolver: %w", err)
+	}
+
+	serialized, err := prototext.MarshalOptions{Multiline: true, Resolver: resolver}.Marshal(raw.Interface())
+	if err == nil {
+		stableFmt, err := parser.Format(serialized)
+		if err == nil {
+			serializedProto.Comments = strings.Split(string(stableFmt), "\n")
+		}
+	}
+
+	return &serializedProto, nil
 }
