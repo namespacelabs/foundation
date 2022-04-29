e39b57063ff23e0320dea97d6584e3115d2589ef
Hugo Santos <hugo@namespacelabs.com>
2022-04-29 22:54:20 +0200

    frontend/cuefrontend: explicitly call out if a package does not exist.

--- internal/frontend/cuefrontend/phase0.go
+++ internal/frontend/cuefrontend/phase0.go
@@ -6,8 +6,11 @@ package cuefrontend
 
 import (
 	"context"
+	"io/fs"
+	"os"
 
 	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/fnfs"
 	"namespacelabs.dev/foundation/internal/frontend/fncue"
 	"namespacelabs.dev/foundation/workspace"
 	"namespacelabs.dev/foundation/workspace/tasks"
@@ -16,6 +19,15 @@ import (
 func parsePackage(ctx context.Context, evalctx *fncue.EvalCtx, pl workspace.EarlyPackageLoader, loc workspace.Location) (*fncue.Partial, error) {
 	var v *fncue.Partial
 	if err := tasks.Action("cue.package.parse").Scope(loc.PackageName).Run(ctx, func(ctx context.Context) error {
+		if st, err := fs.Stat(fnfs.Local(loc.Module.Abs()), loc.Rel()); err != nil {
+			if os.IsNotExist(err) {
+				return fnerrors.UserError(nil, "%s: package does not exist", loc.PackageName)
+			}
+			return err
+		} else if !st.IsDir() {
+			return fnerrors.UserError(loc, "expected a directory")
+		}
+
 		firstPass, err := evalctx.Eval(ctx, loc.PackageName.String())
 		if err != nil {
 			return fnerrors.Wrapf(loc, err, "parsing package")
