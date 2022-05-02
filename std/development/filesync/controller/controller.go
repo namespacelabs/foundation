9fa04c8c91b660c9f2e79466a8fb6a2aa88752f3
Hugo Santos <hugo@namespacelabs.com>
2022-05-02 16:32:53 +0200

    development/filesync: introduce controller which sub-processes a server. (#315)
    This relies on the fact that foundation respects flag ordering. Node-based
    flags are always appended to the list. So a language can set Command to
    `filesync-controller`, and args to `[]string{root, fmt.Sprintf("%d", port),
    originalCommand}` and we'll happily schedule the controller. Which then happily
    starts the specified server, passing along any additional flags.
    
    This is an alternative on the considered sidecar, as the sidecar requires a
    shared volume to be set between the containers. And that would require copying
    the root into the shared volume, adding non-trivial startup time.
    
    For #313

---
+++ std/development/filesync/controller/controller.go
@@ -0,0 +1,52 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
+// available at http://github.com/namespacelabs/foundation
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+
+	"namespacelabs.dev/foundation/std/development/filesync"
+)
+
+func main() {
+	// Args structure is [root] [port] [server provided flags]
+	log.SetFlags(log.Lmicroseconds | log.LstdFlags)
+
+	if len(os.Args) < 4 {
+		log.Fatal("must be passed at least root, port and server")
+	}
+
+	root := os.Args[1]
+	portStr := os.Args[2]
+	command := os.Args[3]
+	args := os.Args[4:]
+
+	port, err := strconv.ParseInt(portStr, 10, 32)
+	if err != nil {
+		log.Fatalf("failed to parse port: %v", err)
+	}
+
+	go filesync.StartFileSyncServer(&filesync.FileSyncConfiguration{
+		RootDir: root,
+		Port:    int32(port),
+	})
+
+	cmd := exec.Command(command, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// Directory and env are inherited.
+
+	if err := cmd.Run(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+
+		log.Fatal(err)
+	}
+}
