f80f4603bdc2a73fa11c55acb6ee95b5b942ba0c
Nik Zherebtsov <nz@namespacelabs.com>
2022-05-03 14:56:34 +0200

    Updating license.

--- languages/nodejs/testdata/extensions/batchformatter/impl.ts
+++ languages/nodejs/testdata/extensions/batchformatter/impl.ts
@@ -1,3 +1,7 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
+// available at http://github.com/namespacelabs/foundation
+
 import { BatchFormatterDeps, ExtensionDeps, ProvideBatchFormatter } from "./deps.fn";
 import { BatchFormatter } from "./formatter";
 import { InputData } from "./input_pb";
