31cb985b671245b06cb4b7e01e2d7db9000ab3f5
Niklas Gehlen <niklas@namespacelabs.com>
2022-05-04 09:37:38 +0200

    skeleton for service

--- internal/cli/cmd/create/service.go
+++ internal/cli/cmd/create/service.go
@@ -6,9 +6,14 @@ package create
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/spf13/cobra"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
+	"namespacelabs.dev/foundation/internal/console"
+	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/fnfs"
+	"namespacelabs.dev/foundation/workspace/source/codegen"
 )
 
 func newServiceCmd() *cobra.Command {
@@ -17,7 +22,18 @@ func newServiceCmd() *cobra.Command {
 		Short: "Creates a service.",
 
 		RunE: fncobra.RunE(func(ctx context.Context, args []string) error {
-			return nil
+			root, loc, err := targetPackage(ctx, args, "server")
+			if err != nil {
+				return err
+			}
+
+			// TODO add file creation
+
+			return codegen.ForLocations(ctx, root, []fnfs.Location{loc}, func(e codegen.GenerateError) {
+				w := console.Stderr(ctx)
+				fmt.Fprintf(w, "%s: %s failed:\n", e.PackageName, e.What)
+				fnerrors.Format(w, true, e.Err)
+			})
 
 		}),
 	}
