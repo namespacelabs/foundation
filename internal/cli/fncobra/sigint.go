c27653b7999d5f411156e429c2cbec4728b2f70b
Hugo Santos <hugo@namespacelabs.com>
2022-05-05 21:46:28 +0200

    internal/cli: give cleanups 2 seconds to run after Ctrl-C (SIGINT).

--- internal/cli/fncobra/sigint.go
+++ internal/cli/fncobra/sigint.go
@@ -9,20 +9,29 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"go.uber.org/atomic"
 )
 
 func WithSigIntCancel(ctx context.Context) (context.Context, func()) {
 	ctx, cancel := context.WithCancel(ctx)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
+	cancelled := atomic.NewBool(false)
+
 	go func() {
 		select {
 		case <-c:
-			cancel()
+			if cancelled.CAS(false, true) {
+				cancel()
 
-			// Give cleanups a moment to run, and then force exit.
-			time.Sleep(10 * time.Second)
-			os.Exit(2)
+				// Give cleanups a moment to run, and then force exit.
+				time.Sleep(2 * time.Second)
+				os.Exit(2)
+			} else {
+				// Already cancelled, exit immediately.
+				os.Exit(3)
+			}
 		case <-ctx.Done():
 		}
 	}()
