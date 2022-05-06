3745316d18a3489b70900bb3b13f0f0393d9bb75
Venkat Karun <vkarun@namespacelabs.com>
2022-05-06 16:11:57 -0300

    correct `ResolveConfig`

--- runtime/kubernetes/client/kubeclient.go
+++ runtime/kubernetes/client/kubeclient.go
@@ -107,6 +107,16 @@ func MakeResourceSpecificClient(resource string, cfg *restclient.Config) (restcl
 }
 
 func ResolveConfig(env ops.Environment) (*restclient.Config, error) {
+	if x, ok := env.(interface {
+		KubeconfigProvider() (KubeconfigProvider, error)
+	}); ok {
+		provider, err := x.KubeconfigProvider()
+		if err != nil {
+			return nil, err
+		}
+		return NewRestConfigFromHostEnv(provider)
+	}
+
 	cfg, err := ComputeHostEnv(env.DevHost(), env.Proto())
 	if err != nil {
 		return nil, err
