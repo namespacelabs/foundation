e696f5062e8126eab70cb4aefe41f0f291e23898
Kamil Anikiej <kamil@namespacelabs.com>
2022-05-09 14:47:08 +0200

    fn generate: topological sort (#342)
    * fn generate: topological sort
    
    * depaware update
    
    * two schema listings (one for protos, one for the rest)
    
    * Sync with main
    
    * Cleanin
    
    * wrap TransformNode with LoadPackageReferences
    
    * Addressing comments.
    
    * Addressing comments.

--- internal/cli/cmd/generate.go
+++ internal/cli/cmd/generate.go
@@ -6,12 +6,17 @@ package cmd
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/philopon/go-toposort"
 	"github.com/spf13/cobra"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
 	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/fnfs"
+	"namespacelabs.dev/foundation/internal/frontend/cuefrontend"
+	"namespacelabs.dev/foundation/internal/frontend/fncue"
+	"namespacelabs.dev/foundation/internal/uniquestrings"
+	"namespacelabs.dev/foundation/schema"
 	"namespacelabs.dev/foundation/workspace"
 	"namespacelabs.dev/foundation/workspace/module"
 	"namespacelabs.dev/foundation/workspace/source/codegen"
@@ -30,19 +35,100 @@ func NewGenerateCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
+			if err := generateProtos(ctx, root); err != nil {
+				return err
+			}
 
 			list, err := workspace.ListSchemas(ctx, root)
 			if err != nil {
 				return err
 			}
-
-			return codegen.ForLocations(ctx, root, list.Locations, func(e codegen.GenerateError) {
+			// Generate code.
+			return codegen.ForLocationsGenCode(ctx, root, list.Locations, func(e codegen.GenerateError) {
 				w := console.Stderr(ctx)
-				fmt.Fprintf(w, "%s: %s failed:\n", e.PackageName, e.What)
 				fnerrors.Format(w, true, e.Err)
 			})
 		}),
 	}
 
 	return cmd
+}
+
+func generateProtos(ctx context.Context, root *workspace.Root) error {
+	list, err := workspace.ListSchemasWithOpt(ctx, root, workspace.ListSchemaOpt{NodeOnly: true})
+	if err != nil {
+		return err
+	}
+	pl := workspace.NewPackageLoader(root)
+	wl := cuefrontend.WorkspaceLoader{PackageLoader: pl}
+
+	cuePackages := make(map[string]*fncue.CuePackage) // Cue packages by PackageName.
+	for _, loc := range list.Locations {
+		if err := fncue.CollectImports(ctx, wl, loc.AsPackageName().String(), cuePackages); err != nil {
+			return err
+		}
+	}
+	imports := map[schema.PackageName]uniquestrings.List{}
+	for packageName, cp := range cuePackages {
+		l := &uniquestrings.List{}
+		for _, imp := range cp.Imports {
+			l.Add(imp)
+		}
+		imports[schema.PackageName(packageName)] = *l
+	}
+	topoSorted, err := topoSortNodes(list.Locations, imports)
+	if err != nil {
+		return err
+	}
+	return codegen.ForLocationsGenProto(ctx, root, topoSorted, func(e codegen.GenerateError) {
+		w := console.Stderr(ctx)
+		fnerrors.Format(w, true, e.Err)
+	})
+}
+
+func topoSortNodes(nodes []fnfs.Location, imports map[schema.PackageName]uniquestrings.List) ([]fnfs.Location, error) {
+	// Gather all the possible nodes into a set.
+	all := &uniquestrings.List{}
+	for _, from := range nodes {
+		parent := from.AsPackageName()
+		all.Add(parent.String())
+		if imp, ok := imports[parent]; ok {
+			for _, i := range imp.Strings() {
+				all.Add(i)
+			}
+		}
+	}
+	graph := toposort.NewGraph(all.Len())
+
+	for _, n := range all.Strings() {
+		graph.AddNode(n)
+	}
+	for _, from := range nodes {
+		parent := from.AsPackageName()
+		if children, ok := imports[parent]; ok {
+			for _, child := range children.Strings() {
+				// First children then parents.
+				graph.AddEdge(child, parent.String())
+			}
+		}
+	}
+
+	result, solved := graph.Toposort()
+	if !solved {
+		return nil, fnerrors.InternalError("dependency graph has a cycle")
+	}
+
+	// Maps package name to the original place in list.Locations.
+	packageToLoc := map[schema.PackageName]fnfs.Location{}
+	for _, loc := range nodes {
+		packageToLoc[loc.AsPackageName()] = loc
+	}
+	end := make([]fnfs.Location, 0, len(nodes))
+	for _, k := range result {
+		if loc, ok := packageToLoc[schema.PackageName(k)]; ok {
+			end = append(end, loc)
+		}
+	}
+
+	return end, nil
 }
