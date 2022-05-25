cfdddad88e0284ce258b30012c8d207f8d4af579
Hugo Santos <hugo@namespacelabs.com>
2022-05-25 22:37:34 +0200

    internal/cli/cmd: updated login to tui.Ask.

--- internal/cli/cmd/login.go
+++ internal/cli/cmd/login.go
@@ -5,14 +5,13 @@
 package cmd
 
 import (
-	"bufio"
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
+	"namespacelabs.dev/foundation/internal/console/tui"
 	"namespacelabs.dev/foundation/internal/fnapi"
 )
 
@@ -25,26 +24,21 @@ func NewLoginCmd() *cobra.Command {
 		Args:  cobra.NoArgs,
 
 		RunE: fncobra.RunE(func(ctx context.Context, args []string) error {
-			out := console.Stdout(ctx)
-			fmt.Fprintln(out, "Open the following URL in your browser, and then copy-paste the resulting code:")
-			fmt.Fprintln(out)
-			fmt.Fprintln(out, "  ", loginUrl)
-			fmt.Fprintln(out)
-
-			done := console.EnterInputMode(ctx, "Code: ")
-			defer done()
-
-			scanner := bufio.NewScanner(os.Stdin)
-			if scanner.Scan() {
-				username, err := fnapi.StoreUser(ctx, scanner.Text())
-				if err != nil {
-					return err
-				}
-
-				fmt.Println()
-				fmt.Printf("Hi %s, you are now logged in, have a nice day.\n", username)
+			code, err := tui.Ask(ctx, "Login to Foundation", fmt.Sprintf("In order to login, open the following URL in your browser, and then copy-paste the resulting code:\n\n  %s", loginUrl), "Code")
+			if err != nil {
+				return err
 			}
 
+			if code == "" {
+				return context.Canceled
+			}
+
+			username, err := fnapi.StoreUser(ctx, code)
+			if err != nil {
+				return err
+			}
+
+			fmt.Fprintf(console.Stdout(ctx), "\nHi %s, you are now logged in, have a nice day.\n", username)
 			return nil
 		}),
 	}
