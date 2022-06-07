1d0d396d750c393aee4e5599f474cf93730b3882
Venkat Karun <vkarun@namespacelabs.com>
2022-06-07 17:54:00 -0300

    address review comments

--- std/networking/gateway/controller/main.go
+++ std/networking/gateway/controller/main.go
@@ -15,25 +15,20 @@ import (
 )
 
 var (
-	l      Logger
-	port   uint
-	nodeID string
-)
-
-func init() {
-	l = Logger{}
-
-	flag.BoolVar(&l.Debug, "debug", false, "Enable xDS server debug logging")
+	debug = flag.Bool("debug", false, "Enable xDS server debug logging")
 
 	// The port that this xDS server listens on
-	flag.UintVar(&port, "port", 18000, "xDS management server port")
+	port = flag.Uint("port", 18000, "xDS management server port")
 
 	// Tell Envoy to use this Node ID
-	flag.StringVar(&nodeID, "nodeID", "test-id", "Node ID")
-}
+	nodeID = flag.String("nodeID", "test-id", "Node ID")
+)
 
 func main() {
 	flag.Parse()
+
+	l := Logger{}
+	l.Debug = *debug
 
 	// Create a cache
 	cache := cache.NewSnapshotCache(false, cache.IDHash{}, l)
@@ -47,7 +42,7 @@ func main() {
 	l.Debugf("will serve snapshot %+v", snapshot)
 
 	// Add the snapshot to the cache
-	if err := cache.SetSnapshot(context.Background(), nodeID, snapshot); err != nil {
+	if err := cache.SetSnapshot(context.Background(), *nodeID, snapshot); err != nil {
 		l.Errorf("snapshot error %q for %+v", err, snapshot)
 		os.Exit(1)
 	}
@@ -56,5 +51,5 @@ func main() {
 	ctx := context.Background()
 	cb := &test.Callbacks{Debug: l.Debug}
 	srv := server.NewServer(ctx, cache, cb)
-	RunServer(ctx, srv, port)
+	RunServer(ctx, srv, *port)
 }
