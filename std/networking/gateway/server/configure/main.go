bf297a8c75a70f0d7dfe82b2f05f567fbc6ba3ca
Venkat Karun <vkarun@namespacelabs.com>
2022-06-17 14:29:09 -0300

    nochangelog: envoy doc reference

--- std/networking/gateway/server/configure/main.go
+++ std/networking/gateway/server/configure/main.go
@@ -135,7 +135,7 @@ func (configuration) Apply(ctx context.Context, req configure.StackRequest, out 
 	envoyArgs := []string{"-c", filepath.Join("/config/", filename)}
 	// Envoy uses shared memory regions during hot restarts and this flag guarantees that
 	// shared memory regions do not conflict when there are multiple running Envoy instances
-	// on the same machine.
+	// on the same machine. See https://www.envoyproxy.io/docs/envoy/latest/operations/cli#cmdoption-use-dynamic-base-id.
 	envoyArgs = append(envoyArgs, "--use-dynamic-base-id")
 
 	out.Extensions = append(out.Extensions, kubedef.ExtendContainer{
