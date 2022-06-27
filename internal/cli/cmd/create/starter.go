0ef03d1bbfb462ec6b16895003773c81ad1331d0
Nik Zherebtsov <nz@namespacelabs.com>
2022-06-27 18:28:08 +0200

    `starter`: supporting generating a workspace in the current directory.

--- internal/cli/cmd/create/starter.go
+++ internal/cli/cmd/create/starter.go
@@ -5,8 +5,10 @@
 package create
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"html/template"
 	"io"
 	"os"
 	"strings"
@@ -15,6 +17,7 @@ import (
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
 	"namespacelabs.dev/foundation/internal/console/colors"
+	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/fnfs"
 )
 
@@ -28,20 +31,31 @@ const (
 	webServerPkg   = "server/web"
 	webServerName  = "WebServer"
 	readmeFilePath = "README.md"
-	readmeTemplate = `Your starter Namespace project has been generated!
+)
+
+var (
+	readmeTemplate = template.Must(template.New("readme").Parse(`Your starter Namespace project has been generated!
 
 Next steps:
 
-- Switch to the project directory: ` + "`" + `cd %s` + "`" + `
+{{if .Dir -}}
+- Switch to the project directory: ` + "`" + `cd {{.Dir}}` + "`" + `{{end}}
 - Run ` + "`" + `ns prepare local` + "`" + ` to prepare the local dev environment.
-- Run ` + "`" + `ns dev %s` + "`" + ` to start the server stack in the development mode with hot reload.
-`
+- Run ` + "`" + `ns dev {{.ServerPkg}}` + "`" + ` to start the server stack in the development mode with hot reload.
+`))
 )
+
+type readmeTmplOpts struct {
+	Dir       string
+	ServerPkg string
+}
 
 func newStarterCmd(runCommand func(ctx context.Context, args []string) error) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "starter",
-		Short: "Creates a new workspace in a new directory from a template.",
+		Short: "Creates a new workspace from a template.",
+		Long:  "Creates a new workspace from a template (Go server + Web server). Creates a directory from the last part of the workspace name by default. Use the second argument to customize the directory or specify '.' to generate project the current directory.",
+		Args:  cobra.RangeArgs(1, 2),
 	}
 
 	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
@@ -50,18 +64,27 @@ func newStarterCmd(runCommand func(ctx context.Context, args []string) error) *c
 			return err
 		}
 
-		nameParts := strings.Split(workspaceName, "/")
-		dirName := nameParts[len(nameParts)-1]
-		if err := os.MkdirAll(dirName, 0755); err != nil {
-			return err
-		}
-		if err := os.Chdir(dirName); err != nil {
-			return err
-		}
-
 		stdout := console.Stdout(ctx)
 
-		printConsoleCmd(ctx, stdout, fmt.Sprintf("mkdir %s; cd %s", dirName, dirName))
+		createDir := len(args) < 2 || args[1] != "."
+
+		dirName := ""
+		if createDir {
+			if len(args) >= 2 {
+				dirName = args[1]
+			} else {
+				nameParts := strings.Split(workspaceName, "/")
+				dirName = nameParts[len(nameParts)-1]
+			}
+			if err := os.MkdirAll(dirName, 0755); err != nil {
+				return err
+			}
+			if err := os.Chdir(dirName); err != nil {
+				return err
+			}
+
+			printConsoleCmd(ctx, stdout, fmt.Sprintf("mkdir %s; cd %s", dirName, dirName))
+		}
 
 		starterCmds := []starterCmd{
 			{
@@ -109,18 +132,7 @@ func newStarterCmd(runCommand func(ctx context.Context, args []string) error) *c
 			}
 		}
 
-		readmeContent := fmt.Sprintf(readmeTemplate, dirName, webServerPkg)
-		cwd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		if err := writeFileIfDoesntExist(ctx, fnfs.ReadWriteLocalFS(cwd), readmeFilePath, readmeContent); err != nil {
-			return err
-		}
-
-		fmt.Fprintf(stdout, "\n\n%s\n", colors.Ctx(ctx).Highlight.Apply(readmeContent))
-
-		return nil
+		return generateAndPrintReadme(ctx, stdout, dirName)
 	})
 
 	return cmd
@@ -133,4 +145,30 @@ type starterCmd struct {
 
 func printConsoleCmd(ctx context.Context, out io.Writer, text string) {
 	fmt.Fprintf(out, "\n> %s\n", colors.Ctx(ctx).Highlight.Apply(text))
+}
+
+func generateAndPrintReadme(ctx context.Context, out io.Writer, dir string) error {
+	data := readmeTmplOpts{
+		Dir:       dir,
+		ServerPkg: webServerPkg,
+	}
+
+	var body bytes.Buffer
+	if err := readmeTemplate.Execute(&body, data); err != nil {
+		return fnerrors.InternalError("failed to apply template: %w", err)
+	}
+
+	readmeContent := body.String()
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err := writeFileIfDoesntExist(ctx, fnfs.ReadWriteLocalFS(cwd), readmeFilePath, readmeContent); err != nil {
+		return err
+	}
+
+	fmt.Fprintf(out, "\n\n%s\n", colors.Ctx(ctx).Highlight.Apply(readmeContent))
+
+	return nil
 }
