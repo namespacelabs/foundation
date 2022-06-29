d47ee684e166336e7865e1eaf00eb5cef0a0e85f
Nikolay Zherebtsov <nz@namespacelabs.com>
2022-06-29 13:31:28 +0200

    Always including "protobufts" to the "buf" image but making it much smaller by building in a separate image. (#697)

--- internal/sdk/buf/image/image.go
+++ internal/sdk/buf/image/image.go
@@ -39,22 +39,16 @@ type bufDef struct {
 	Prebuilt string `json:"prebuilt"`
 }
 
-type Opts struct {
-	// protobuf-ts is expensive, so we reserve it being built to when it's
-	// actually needed (e.g. to do codegen for ts).
-	IncludeProtobufTs bool
-}
-
 func Prebuilt(target specs.Platform) llb.State {
 	versions := loadVersions()
 	return llbutil.Image(versions.Buf.Prebuilt, target)
 }
 
-func ImageSource(platform specs.Platform, opts Opts) llb.State {
+func ImageSource(platform specs.Platform) llb.State {
 	versions := loadVersions()
 
 	golangImage := pins.Image(versions.Buf.Go)
-	alpineImage := pins.Default("alpine")
+	nodeImage := pins.Default("node")
 
 	gobase := llbutil.Image(golangImage, platform).
 		AddEnv("CGO_ENABLED", "0").
@@ -86,32 +80,49 @@ func ImageSource(platform specs.Platform, opts Opts) llb.State {
 		copies = append(copies, llbutil.CopyFrom(out, "/go/bin/"+bin, "/bin/"+bin))
 	}
 
-	target := llbutil.Image(alpineImage, platform)
-	target = target.Run(llb.Shlex(fmt.Sprintf("apk add --no-cache protoc=%s", versions.Protoc))).Root()
-
-	if opts.IncludeProtobufTs {
-		// Compiling forked protobuf-ts from sources. Takes ~1 minute.
-		// This is temporary, eventually we need to migrate to a published version.
-		target = target.Run(llb.Shlex("apk add --no-cache git make npm")).Root().
-			Run(llb.Shlex("git clone https://github.com/namespacelabs/protobuf-ts.git")).Root().
-			// Creating a temporary local branch for the pinned commit
-			Run(llb.Shlex(fmt.Sprintf("git checkout -b TmpBranchForPinnedCommit %s", versions.ProtobufTs)),
-				llb.Dir("protobuf-ts")).Root().
-			Run(llb.Shlex("make npm-install lerna-bootstrap"),
-				llb.Dir("protobuf-ts")).Root().
-			Run(llb.Shlex("make build"),
-				llb.Dir("protobuf-ts/packages/runtime")).Root().
-			Run(llb.Shlex("make build"),
-				llb.Dir("protobuf-ts/packages/runtime-rpc")).Root().
-			Run(llb.Shlex("make build"),
-				llb.Dir("protobuf-ts/packages/plugin-framework")).Root().
-			Run(llb.Shlex("make build"),
-				llb.Dir("protobuf-ts/packages/plugin")).Root().
-			Run(llb.Shlex("ls"),
-				llb.Dir("protobuf-ts/packages/plugin/bin")).Root()
+	baseProtobufTsImage := buildProtobufTsImage(platform)
+	protobufTsPaths := [][]string{
+		{"protobuf-ts/packages/plugin/bin", "protobuf-ts/packages/plugin/bin"},
+		{"protobuf-ts/packages/plugin/build", "protobuf-ts/packages/plugin/build"},
+		{"protobuf-ts/packages/plugin/package.json", "protobuf-ts/packages/plugin/package.json"},
+		{"protobuf-ts/packages/plugin-framework/build/commonjs", "protobuf-ts/packages/plugin/node_modules/@protobuf-ts/plugin-framework"},
+		{"protobuf-ts/packages/runtime/build/commonjs", "protobuf-ts/packages/plugin/node_modules/@protobuf-ts/runtime"},
+		{"protobuf-ts/packages/runtime-rpc/build/commonjs", "protobuf-ts/packages/plugin/node_modules/@protobuf-ts/runtime-rpc"},
+		{"protobuf-ts/packages/plugin/node_modules/typescript/lib/typescript.js", "protobuf-ts/packages/plugin/node_modules/typescript/index.js"},
+	}
+	for _, pair := range protobufTsPaths {
+		copies = append(copies, llbutil.CopyFrom(baseProtobufTsImage, pair[0], pair[1]))
 	}
 
+	target := llbutil.Image(nodeImage, platform)
 	return target.With(copies...)
+}
+
+func buildProtobufTsImage(platform specs.Platform) llb.State {
+	versions := loadVersions()
+
+	alpineImage := pins.Default("alpine")
+	target := llbutil.Image(alpineImage, platform)
+
+	// Compiling forked protobuf-ts from sources. Takes ~1 minute.
+	// This is temporary, eventually we need to migrate to a published version.
+	return target.Run(llb.Shlex("apk add --no-cache git make npm")).Root().
+		Run(llb.Shlex("git clone https://github.com/namespacelabs/protobuf-ts.git")).Root().
+		// Creating a temporary local branch for the pinned commit
+		Run(llb.Shlex(fmt.Sprintf("git checkout -b TmpBranchForPinnedCommit %s", versions.ProtobufTs)),
+			llb.Dir("protobuf-ts")).Root().
+		Run(llb.Shlex("make npm-install lerna-bootstrap"),
+			llb.Dir("protobuf-ts")).Root().
+		Run(llb.Shlex("make build"),
+			llb.Dir("protobuf-ts/packages/runtime")).Root().
+		Run(llb.Shlex("make build"),
+			llb.Dir("protobuf-ts/packages/runtime-rpc")).Root().
+		Run(llb.Shlex("make build"),
+			llb.Dir("protobuf-ts/packages/plugin-framework")).Root().
+		Run(llb.Shlex("make build"),
+			llb.Dir("protobuf-ts/packages/plugin")).Root().
+		Run(llb.Shlex("ls"),
+			llb.Dir("protobuf-ts/packages/plugin/bin")).Root()
 }
 
 func loadVersions() versionsJSON {
--- internal/sdk/buf/protogen.go
+++ internal/sdk/buf/protogen.go
@@ -22,7 +22,6 @@ import (
 	"namespacelabs.dev/foundation/internal/engine/ops"
 	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/fnfs/tarfs"
-	"namespacelabs.dev/foundation/internal/sdk/buf/image"
 	"namespacelabs.dev/foundation/runtime/tools"
 	"namespacelabs.dev/foundation/schema"
 	"namespacelabs.dev/foundation/workspace/compute"
@@ -38,13 +37,7 @@ func MakeProtoSrcs(ctx context.Context, env ops.Environment, request map[schema.
 	keys := maps.Keys(request)
 	slices.Sort(keys)
 
-	opts := image.Opts{}
-
-	if _, ok := request[schema.Framework_NODEJS]; ok {
-		opts.IncludeProtobufTs = true
-	}
-
-	base := State(platform, opts)
+	base := State(platform)
 
 	out := llb.Scratch()
 	for _, fmwk := range keys {
