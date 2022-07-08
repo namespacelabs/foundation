1ad4ba5e00c8347a263d8d65407036d8c1b578d1
Venkat Karun <102962107+vkarunx@users.noreply.github.com>
2022-07-08 07:48:49 -0300

    nochangelog: duplicate license messages

--- universe/networking/k8s-event-exporter/configure/main.go
+++ universe/networking/k8s-event-exporter/configure/main.go
@@ -2,10 +2,6 @@
 // Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
 // available at http://github.com/namespacelabs/foundation
 
-// Copyright 2022 Namespace Labs Inc; All rights reserved.
-// Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
-// available at http://github.com/namespacelabs/foundation
-
 package main
 
 import (
