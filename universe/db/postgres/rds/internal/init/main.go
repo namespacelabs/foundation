8c542444b3c927015ce47e9ed6d7e23038289d24
Niklas Gehlen <niklas@namespacelabs.com>
2022-07-15 16:23:05 +0200

    wire db names

--- universe/db/postgres/rds/internal/init/main.go
+++ universe/db/postgres/rds/internal/init/main.go
@@ -15,7 +15,9 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	awsrds "github.com/aws/aws-sdk-go-v2/service/rds"
+	"golang.org/x/sync/errgroup"
 	"namespacelabs.dev/foundation/internal/console"
+	"namespacelabs.dev/foundation/universe/db/postgres"
 )
 
 const connBackoff = 1 * time.Second
@@ -32,6 +34,53 @@ var (
 	class   = "db.m5d.xlarge"
 	iops    = int32(3000)
 )
+
+// TODO dedup
+func readConfigs() ([]*postgres.Database, error) {
+	dbs := []*postgres.Database{}
+
+	for _, path := range flag.Args() {
+		file, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("unable to read file %s: %v", path, err)
+		}
+
+		db := &postgres.Database{}
+		if err := json.Unmarshal(file, db); err != nil {
+			return nil, err
+		}
+		dbs = append(dbs, db)
+	}
+
+	return dbs, nil
+}
+
+// TODO dedup
+func prepareDatabase(ctx context.Context, rdscli *awsrds.Client, db *postgres.Database, user, password string) error {
+	input := &awsrds.CreateDBClusterInput{
+		Engine:                 &engine, // Also set engine version?
+		AllocatedStorage:       &storage,
+		DBClusterInstanceClass: &class,
+		Iops:                   &iops,
+		MasterUsername:         &user,
+		MasterUserPassword:     &password,
+		DatabaseName:           &db.Name,
+	}
+
+	out, err := rdscli.CreateDBCluster(ctx, input)
+	if err != nil {
+		return fmt.Errorf("failed to create database cluster: %v", err)
+	}
+
+	// Debug
+	serialized, err := json.MarshalIndent(out, "", " ")
+	if err != nil {
+		return fmt.Errorf("failed to mashal response: %v", err)
+	}
+	fmt.Fprintf(console.Stdout(ctx), "rdscli.CreateDBCluster:\n%s\n", string(serialized))
+
+	return nil
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
@@ -62,28 +111,22 @@ func main() {
 	}
 	rdscli := awsrds.NewFromConfig(awsCfg)
 
-	input := &awsrds.CreateDBClusterInput{
-		Engine:                 &engine, // Also set engine version?
-		AllocatedStorage:       &storage,
-		DBClusterInstanceClass: &class,
-		Iops:                   &iops,
-		MasterUsername:         &user,
-		MasterUserPassword:     &password,
-	}
-
-	out, err := rdscli.CreateDBCluster(ctx, input)
+	dbs, err := readConfigs()
 	if err != nil {
-		log.Fatalf("failed to create database cluster: %v", err)
+		log.Fatalf("%v", err)
 	}
 
-	// Debug
-	serialized, err := json.MarshalIndent(out, "", " ")
-	if err != nil {
-		log.Fatalf("failed to mashal response: %v", err)
+	eg, ctx := errgroup.WithContext(ctx)
+	for _, db := range dbs {
+		db := db // Close db
+		eg.Go(func() error {
+			return prepareDatabase(ctx, rdscli, db, user, password)
+		})
 	}
-	fmt.Fprintf(console.Stdout(ctx), "rdscli.CreateDBCluster:\n%s\n", string(serialized))
 
-	// TODO now init the DBs
+	if err := eg.Wait(); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Printf("postgres init completed")
 }
