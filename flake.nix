1afc318d1a2c29446255f980c2165e2fd5f5eddd
Hugo Santos <hugo@namespacelabs.com>
2022-07-25 15:50:38 +0200

    flake.nix: remove alejandra, use nix-fmt instead.

--- flake.nix
+++ flake.nix
@@ -2,15 +2,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    alejandra.url = "github:kamadorueda/alejandra/1.1.0";
-    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    alejandra,
     flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -37,9 +35,6 @@
             aws-iam-authenticator
             eksctl
             google-cloud-sdk
-          ]
-          ++ [
-            alejandra.defaultPackage.${system} # For nix file formatting. Bit meta.
           ];
       };
     });
