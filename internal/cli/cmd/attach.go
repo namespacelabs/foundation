b7495d1086b03b01d2ed247f47e04fc216ea2720
Nikolay Zherebtsov <nz@namespacelabs.com>
2022-07-27 18:42:40 +0200

    Migrating "attach" and "use psql/pgdump/pgrestore" to the new cmd api. (#810)

--- internal/cli/cmd/attach.go
+++ internal/cli/cmd/attach.go
@@ -14,29 +14,23 @@ import (
 	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/logs/logtail"
 	"namespacelabs.dev/foundation/internal/observers"
-	"namespacelabs.dev/foundation/internal/stack"
-	"namespacelabs.dev/foundation/provision/config"
 	"namespacelabs.dev/foundation/provision/deploy/view"
 	"namespacelabs.dev/foundation/runtime"
 	"namespacelabs.dev/foundation/schema"
 )
 
 func NewAttachCmd() *cobra.Command {
-	h := hydrateArgs{envRef: "dev", rehydrate: true}
+	var res hydrateResult
 
-	cmd := &cobra.Command{
-		Use:   "attach",
-		Short: "Attaches to the specified environment, of the specified servers.",
-		Args:  cobra.ArbitraryArgs,
-
-		RunE: fncobra.RunE(func(ctx context.Context, args []string) error {
-			res, err := h.ComputeStack(ctx, args)
-			if err != nil {
-				return err
-			}
-
+	return fncobra.
+		Cmd(&cobra.Command{
+			Use:   "attach",
+			Short: "Attaches to the specified environment, of the specified servers.",
+			Args:  cobra.ArbitraryArgs}).
+		With(parseHydrationWithDeps(&res, &fncobra.ParseLocationsOpts{DefaultToAllWhenEmpty: true}, &hydrateOpts{rehydrate: true})...).
+		Do(func(ctx context.Context) error {
 			event := &observers.StackUpdateEvent{
-				Env:   res.Rehydrated.Env,
+				Env:   res.Env.Proto(),
 				Stack: res.Stack,
 				Focus: schema.Strs(res.Focus...),
 			}
@@ -48,7 +42,7 @@ func NewAttachCmd() *cobra.Command {
 				Keybindings: []keyboard.Handler{
 					logtail.Keybinding{
 						LoadEnvironment: func(name string) (runtime.Selector, error) {
-							if name == res.Rehydrated.Env.Name {
+							if name == res.Env.Proto().Name {
 								return res.Env, nil
 							}
 
@@ -68,95 +62,5 @@ func NewAttachCmd() *cobra.Command {
 					return nil
 				},
 			})
-		}),
-	}
-
-	h.Configure(cmd)
-
-	return cmd
-}
-
-type hydrateArgs struct {
-	envRef          string
-	usePackageNames bool
-	rehydrate       bool
-
-	rehydrateOnly bool
-}
-
-type hydrateResult struct {
-	Env        runtime.Selector
-	Stack      *schema.Stack
-	Focus      []schema.PackageName
-	Ingress    []*schema.IngressFragment
-	Rehydrated *config.Rehydrated
-}
-
-func (h *hydrateArgs) Configure(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&h.envRef, "env", h.envRef, "The environment to attach to.")
-	cmd.Flags().BoolVar(&h.usePackageNames, "use_package_names", h.usePackageNames, "Specify servers by using their fully qualified package name instead.")
-	if !h.rehydrateOnly {
-		cmd.Flags().BoolVar(&h.rehydrate, "rehydrate", h.rehydrate, "If set to false, compute stack at head, rather than loading the deployed configuration.")
-	}
-}
-
-func (h *hydrateArgs) ComputeStack(ctx context.Context, args []string) (*hydrateResult, error) {
-	env, err := requireEnv(ctx, h.envRef)
-	if err != nil {
-		return nil, err
-	}
-
-	serverLocs, specified, err := allServersOrFromArgs(ctx, env, h.usePackageNames, args)
-	if err != nil {
-		return nil, err
-	}
-
-	_, servers, err := loadServers(ctx, env, serverLocs, specified)
-	if err != nil {
-		return nil, err
-	}
-
-	var res hydrateResult
-	for _, srv := range servers {
-		res.Focus = append(res.Focus, srv.PackageName())
-	}
-
-	res.Env = env
-
-	if h.rehydrate || h.rehydrateOnly {
-		if len(servers) != 1 {
-			return nil, fnerrors.UserError(nil, "--rehydrate only supports a single server")
-		}
-
-		buildID, err := runtime.For(ctx, env).DeployedConfigImageID(ctx, servers[0].Proto())
-		if err != nil {
-			return nil, err
-		}
-
-		rehydrated, err := config.Rehydrate(ctx, servers[0], buildID)
-		if err != nil {
-			return nil, err
-		}
-
-		res.Stack = rehydrated.Stack
-		res.Ingress = rehydrated.IngressFragments
-		res.Rehydrated = rehydrated
-	} else {
-		stack, err := stack.Compute(ctx, servers, stack.ProvisionOpts{PortRange: runtime.DefaultPortRange()})
-		if err != nil {
-			return nil, err
-		}
-
-		res.Stack = stack.Proto()
-		for _, entry := range stack.Proto().Entry {
-			deferred, err := runtime.ComputeIngress(ctx, env.Proto(), entry, stack.Endpoints)
-			if err != nil {
-				return nil, err
-			}
-
-			res.Ingress = deferred
-		}
-	}
-
-	return &res, nil
+		})
 }
