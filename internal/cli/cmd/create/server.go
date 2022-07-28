fe4f4475e1c9f9e646909a0f996092fbd6e48059
Nikolay Zherebtsov <nz@namespacelabs.com>
2022-07-28 13:08:31 +0200

    Migrating "starter" commands to ParseLocations. (#811)

--- internal/cli/cmd/create/server.go
+++ internal/cli/cmd/create/server.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console/tui"
 	"namespacelabs.dev/foundation/internal/fnerrors"
@@ -22,85 +23,87 @@ import (
 const serverSuffix = "server"
 
 func newServerCmd(runCommand func(ctx context.Context, args []string) error) *cobra.Command {
-	use := "server"
-	cmd := &cobra.Command{
-		Use:   use,
-		Short: "Creates a server.",
-		Args:  cobra.RangeArgs(0, 1),
-	}
+	var (
+		targetPkg targetPkg
+		fmwkFlag  string
+		name      string
+	)
+	grpcServices := []string{}
+	httpServices := []string{}
 
-	fmwkStr := frameworkFlag(cmd)
-	name := cmd.Flags().String("name", "", "Server name.")
-	grpcServices := cmd.Flags().StringArray("with_service", nil, "A service to wire to the server.")
-	httpServices := cmd.Flags().StringArray("with_http_service", nil, "An HTTP service to wire to the server. Format: 'path:package'.")
-
-	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
-		parsedHttpServices := []cue.HttpService{}
-		for _, httpService := range *httpServices {
-			parts := strings.Split(httpService, ":")
-			if len(parts) != 2 {
-				return fnerrors.UserError(nil, "invalid http_services format: %s", httpService)
-			}
-			parsedHttpServices = append(parsedHttpServices, cue.HttpService{
-				Path: parts[0],
-				Pkg:  parts[1],
-			})
-		}
-
-		root, loc, err := targetPackage(ctx, args, use)
-		if err != nil {
-			return err
-		}
-
-		fmwk, err := selectFramework(ctx, "Which framework are your services in?", fmwkStr)
-		if err != nil {
-			return err
-		}
-
-		if fmwk == nil {
-			return context.Canceled
-		}
-
-		var dependencies []string
-		if *fmwk == schema.Framework_GO {
-			if err := runGoInitCmdIfNeeded(ctx, root, runCommand); err != nil {
-				return err
+	return fncobra.
+		Cmd(&cobra.Command{
+			Use:   "server",
+			Short: "Creates a server.",
+			Args:  cobra.RangeArgs(0, 1),
+		}).
+		WithFlags(func(flags *pflag.FlagSet) {
+			flags.StringVar(&name, "name", "", "Server name.")
+			flags.StringArrayVar(&grpcServices, "with_service", nil, "A service to wire to the server.")
+			flags.StringArrayVar(&httpServices, "with_http_service", nil, "An HTTP service to wire to the server. Format: 'path:package'.")
+		}).
+		With(parseTargetPkgWithDeps(&targetPkg, "service")...).
+		With(withFramework(&fmwkFlag)).
+		Do(func(ctx context.Context) error {
+			parsedHttpServices := []cue.HttpService{}
+			for _, httpService := range httpServices {
+				parts := strings.Split(httpService, ":")
+				if len(parts) != 2 {
+					return fnerrors.UserError(nil, "invalid http_services format: %s", httpService)
+				}
+				parsedHttpServices = append(parsedHttpServices, cue.HttpService{
+					Path: parts[0],
+					Pkg:  parts[1],
+				})
 			}
 
-			dependencies = append(dependencies,
-				"namespacelabs.dev/foundation/std/grpc/logging",
-				"namespacelabs.dev/foundation/std/monitoring/tracing/jaeger")
-		}
-
-		if *name == "" {
-			*name, err = tui.Ask(ctx, "How would you like to name your server?",
-				"A server's name is used to generate various production resource names and thus should not contain private information.",
-				serverName(loc))
+			fmwk, err := selectFramework(ctx, "Which framework are your services in?", fmwkFlag)
 			if err != nil {
 				return err
 			}
-		}
 
-		if *name == "" {
-			return context.Canceled
-		}
+			if fmwk == nil {
+				return context.Canceled
+			}
 
-		opts := cue.GenServerOpts{Name: *name, Framework: *fmwk, GrpcServices: *grpcServices, Dependencies: dependencies, HttpServices: parsedHttpServices}
-		if err := cue.CreateServerScaffold(ctx, root.FS(), loc, opts); err != nil {
-			return err
-		}
+			var dependencies []string
+			if *fmwk == schema.Framework_GO {
+				if err := runGoInitCmdIfNeeded(ctx, targetPkg.Root, runCommand); err != nil {
+					return err
+				}
 
-		// Aggregates and prints all accumulated codegen errors on return.
-		var errorCollector fnerrors.ErrorCollector
+				dependencies = append(dependencies,
+					"namespacelabs.dev/foundation/std/grpc/logging",
+					"namespacelabs.dev/foundation/std/monitoring/tracing/jaeger")
+			}
 
-		if err := codegen.ForLocationsGenCode(ctx, root, []fnfs.Location{loc}, errorCollector.Append); err != nil {
-			return err
-		}
+			if name == "" {
+				name, err = tui.Ask(ctx, "How would you like to name your server?",
+					"A server's name is used to generate various production resource names and thus should not contain private information.",
+					serverName(targetPkg.Loc))
+				if err != nil {
+					return err
+				}
+			}
 
-		return errorCollector.Error()
-	})
+			if name == "" {
+				return context.Canceled
+			}
 
-	return cmd
+			opts := cue.GenServerOpts{Name: name, Framework: *fmwk, GrpcServices: grpcServices, Dependencies: dependencies, HttpServices: parsedHttpServices}
+			if err := cue.CreateServerScaffold(ctx, targetPkg.Root.FS(), targetPkg.Loc, opts); err != nil {
+				return err
+			}
+
+			// Aggregates and prints all accumulated codegen errors on return.
+			var errorCollector fnerrors.ErrorCollector
+
+			if err := codegen.ForLocationsGenCode(ctx, targetPkg.Root, []fnfs.Location{targetPkg.Loc}, errorCollector.Append); err != nil {
+				return err
+			}
+
+			return errorCollector.Error()
+		})
 }
 
 func serverName(loc fnfs.Location) string {
