389b60a8638c99c6c0ed4dc4dc25583ef861b768
Nik Zherebtsov <nz@namespacelabs.com>
2022-07-28 17:03:17 +0200

    Migrating "proto-generate" command to ParseLocations. #758

--- internal/cli/cmd/source/bufgenerate.go
+++ internal/cli/cmd/source/bufgenerate.go
@@ -7,42 +7,38 @@ package source
 import (
 	"context"
 	"fmt"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/provision"
 	"namespacelabs.dev/foundation/schema"
-	"namespacelabs.dev/foundation/workspace/module"
 	"namespacelabs.dev/foundation/workspace/source"
 )
 
 func newBufGenerateCmd() *cobra.Command {
-	lang := "go"
+	var (
+		lang string
+		env  provision.Env
+		locs fncobra.Locations
+	)
 
-	cmd := &cobra.Command{
-		Use:     "proto-generate",
-		Short:   "Run buf.build generate on your codebase.",
-		Aliases: []string{"proto-gen", "protogen"},
-
-		RunE: fncobra.RunE(func(ctx context.Context, userArgs []string) error {
-			root, err := module.FindRoot(ctx, ".")
-			if err != nil {
-				return err
-			}
-
-			env, err := provision.RequireEnv(root, "dev")
-			if err != nil {
-				return err
-			}
-
-			clean := make([]string, len(userArgs))
-			for k, str := range userArgs {
-				clean[k] = filepath.Clean(str)
-			}
-
-			if len(clean) == 0 {
-				clean = []string{"."}
+	return fncobra.
+		Cmd(&cobra.Command{
+			Use:     "proto-generate",
+			Short:   "Run buf.build generate on your codebase.",
+			Aliases: []string{"proto-gen", "protogen"},
+		}).
+		WithFlags(func(flags *pflag.FlagSet) {
+			flags.StringVar(&lang, "lang", "go", "Language for proto generation. Supported values: go, typescript.")
+		}).
+		With(
+			fncobra.FixedEnv(&env, "dev"),
+			fncobra.ParseLocations(&locs, &fncobra.ParseLocationsOpts{})).
+		Do(func(ctx context.Context) error {
+			paths := []string{}
+			for _, loc := range locs.Locs {
+				paths = append(paths, loc.RelPath)
 			}
 
 			var fmwk schema.Framework
@@ -55,12 +51,6 @@ func newBufGenerateCmd() *cobra.Command {
 				return fmt.Errorf("unsupported language: %s", lang)
 			}
 
-			return source.GenProtosAtPaths(ctx, env, root, fmwk, clean, root.FS())
-		}),
-	}
-
-	cmd.Flags().StringVar(&lang, "lang", lang,
-		"Language for proto generation. Supported values: go, typescript.")
-
-	return cmd
+			return source.GenProtosAtPaths(ctx, env, env.Root(), fmwk, paths, env.Root().FS())
+		})
 }
