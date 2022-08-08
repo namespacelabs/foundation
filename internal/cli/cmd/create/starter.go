0c899b6424e9325c45992ec6435ed07f56c92693
Nik Zherebtsov <nz@namespacelabs.com>
2022-08-08 13:54:35 +0200

    Migrating `ns starter` to `fncobra.Cmd` for consistency.

--- internal/cli/cmd/create/starter.go
+++ internal/cli/cmd/create/starter.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/muesli/reflow/wordwrap"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
 	"namespacelabs.dev/foundation/internal/console/colors"
@@ -34,122 +35,125 @@ const (
 )
 
 func newStarterCmd(runCommand func(ctx context.Context, args []string) error) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "starter [directory]",
-		Short: "Creates a new workspace from a template.",
-		Long:  "Creates a new workspace from a template (Go server + Web server) in the given directory.",
-		Args:  cobra.MaximumNArgs(1),
-	}
+	var (
+		workspaceName string
+	)
 
-	workspaceName := cmd.Flags().String("workspace_name", "", "Name of the workspace.")
+	return fncobra.
+		Cmd(&cobra.Command{
+			Use:   "starter [directory]",
+			Short: "Creates a new workspace from a template.",
+			Long:  "Creates a new workspace from a template (Go server + Web server) in the given directory.",
+			Args:  cobra.MaximumNArgs(1),
+		}).
+		WithFlags(func(flags *pflag.FlagSet) {
+			flags.StringVar(&workspaceName, "workspace_name", "", "Name of the workspace.")
+		}).
+		DoWithArgs(func(ctx context.Context, args []string) error {
+			stdout := console.Stdout(ctx)
 
-	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
-		stdout := console.Stdout(ctx)
+			fmt.Fprintf(stdout, "\nSetting up a starter project with an api server in Go and a web frontend. It will take a few minutes.\n")
 
-		fmt.Fprintf(stdout, "\nSetting up a starter project with an api server in Go and a web frontend. It will take a few minutes.\n")
+			var err error
+			var dirName string
 
-		var err error
-		var dirName string
+			if workspaceName == "" {
+				workspaceName, err = askWorkspaceName(ctx)
+				if err != nil {
+					return err
+				}
+				if workspaceName == "" {
+					return context.Canceled
+				}
+			}
 
-		if *workspaceName == "" {
-			*workspaceName, err = askWorkspaceName(ctx)
+			if len(args) > 0 {
+				dirName = args[0]
+			} else {
+				nameParts := strings.Split(workspaceName, "/")
+				dirNamePlaceholder := nameParts[len(nameParts)-1]
+				dirName, err = tui.Ask(ctx,
+					"Directory for the new project?",
+					"It can be a relative or an absolute path. Use '.' to generate the project in the current directory.",
+					dirNamePlaceholder)
+				if err != nil {
+					return err
+				}
+			}
+
+			if dirName != "." {
+				if err := os.MkdirAll(dirName, 0755); err != nil {
+					return err
+				}
+
+				fmt.Fprintf(stdout, "\nCreated directory '%s'.\n", dirName)
+
+				if err := os.Chdir(dirName); err != nil {
+					return err
+				}
+
+				printConsoleCmd(ctx, stdout, fmt.Sprintf("cd %s", dirName))
+			}
+
+			starterCmds := []starterCmd{
+				{
+					description: "Bootstrapping the workspace configuration.",
+					args:        []string{"create", "workspace", workspaceName},
+				},
+				{
+					description: fmt.Sprintf("Adding an example Go API service '%s' at '%s'.", goServiceName, goServicePkg),
+					args: []string{"create", "service", goServicePkg,
+						"--framework=go",
+						fmt.Sprintf("--name=%s", goServiceName)},
+				},
+				{
+					description: fmt.Sprintf("Adding an example Go API server '%s' at '%s'.", goServerName, goServerPkg),
+					args: []string{"create", "server", goServerPkg,
+						"--framework=go",
+						fmt.Sprintf("--name=%s", goServerName),
+						fmt.Sprintf("--with_service=%s/%s", workspaceName, goServicePkg),
+					},
+				},
+				{
+					description: fmt.Sprintf("Adding an example Web service '%s' at '%s'.", webServiceName, webServicePkg),
+					args: []string{"create", "service", webServicePkg,
+						"--framework=web",
+						fmt.Sprintf("--with_http_backend=%s/%s", workspaceName, goServerPkg),
+					},
+				},
+				{
+					description: fmt.Sprintf("Adding an example Web server '%s' at '%s'.", webServerName, webServerPkg),
+					args: []string{"create", "server", webServerPkg, "--framework=web",
+						fmt.Sprintf("--name=%s", webServerName),
+						fmt.Sprintf("--with_http_service=/:%s/%s", workspaceName, webServicePkg)},
+				},
+				{
+					description: "Adding an example e2e test.",
+					args: []string{"create", "test", testPkg,
+						fmt.Sprintf("--server=%s/%s", workspaceName, goServerPkg),
+						fmt.Sprintf("--service=%s/%s", workspaceName, goServicePkg)},
+				},
+				{
+					description: "Bringing the language-specific configuration up to date, making it consistent with the Namespace configuration. Downloading language-specific dependencies.\nIt may take a few minutes.",
+					args:        []string{"tidy"},
+				},
+			}
+
+			for _, starterCmd := range starterCmds {
+				printConsoleCmd(ctx, stdout, fmt.Sprintf("ns %s", strings.Join(starterCmd.args, " ")))
+				fmt.Fprintf(stdout, "%s\n\n", wordwrap.String(colors.Ctx(ctx).Comment.Apply(starterCmd.description), 80))
+				if err := runCommand(ctx, starterCmd.args); err != nil {
+					return err
+				}
+			}
+
+			// README.md file content and the content to print to console are slightly different.
+			err = generateAndWriteReadmeFile(ctx, stdout)
 			if err != nil {
 				return err
 			}
-			if *workspaceName == "" {
-				return context.Canceled
-			}
-		}
-
-		if len(args) > 0 {
-			dirName = args[0]
-		} else {
-			nameParts := strings.Split(*workspaceName, "/")
-			dirNamePlaceholder := nameParts[len(nameParts)-1]
-			dirName, err = tui.Ask(ctx,
-				"Directory for the new project?",
-				"It can be a relative or an absolute path. Use '.' to generate the project in the current directory.",
-				dirNamePlaceholder)
-			if err != nil {
-				return err
-			}
-		}
-
-		if dirName != "." {
-			if err := os.MkdirAll(dirName, 0755); err != nil {
-				return err
-			}
-
-			fmt.Fprintf(stdout, "\nCreated directory '%s'.\n", dirName)
-
-			if err := os.Chdir(dirName); err != nil {
-				return err
-			}
-
-			printConsoleCmd(ctx, stdout, fmt.Sprintf("cd %s", dirName))
-		}
-
-		starterCmds := []starterCmd{
-			{
-				description: "Bootstrapping the workspace configuration.",
-				args:        []string{"create", "workspace", *workspaceName},
-			},
-			{
-				description: fmt.Sprintf("Adding an example Go API service '%s' at '%s'.", goServiceName, goServicePkg),
-				args: []string{"create", "service", goServicePkg,
-					"--framework=go",
-					fmt.Sprintf("--name=%s", goServiceName)},
-			},
-			{
-				description: fmt.Sprintf("Adding an example Go API server '%s' at '%s'.", goServerName, goServerPkg),
-				args: []string{"create", "server", goServerPkg,
-					"--framework=go",
-					fmt.Sprintf("--name=%s", goServerName),
-					fmt.Sprintf("--with_service=%s/%s", *workspaceName, goServicePkg),
-				},
-			},
-			{
-				description: fmt.Sprintf("Adding an example Web service '%s' at '%s'.", webServiceName, webServicePkg),
-				args: []string{"create", "service", webServicePkg,
-					"--framework=web",
-					fmt.Sprintf("--with_http_backend=%s/%s", *workspaceName, goServerPkg),
-				},
-			},
-			{
-				description: fmt.Sprintf("Adding an example Web server '%s' at '%s'.", webServerName, webServerPkg),
-				args: []string{"create", "server", webServerPkg, "--framework=web",
-					fmt.Sprintf("--name=%s", webServerName),
-					fmt.Sprintf("--with_http_service=/:%s/%s", *workspaceName, webServicePkg)},
-			},
-			{
-				description: "Adding an example e2e test.",
-				args: []string{"create", "test", testPkg,
-					fmt.Sprintf("--server=%s/%s", *workspaceName, goServerPkg),
-					fmt.Sprintf("--service=%s/%s", *workspaceName, goServicePkg)},
-			},
-			{
-				description: "Bringing the language-specific configuration up to date, making it consistent with the Namespace configuration. Downloading language-specific dependencies.\nIt may take a few minutes.",
-				args:        []string{"tidy"},
-			},
-		}
-
-		for _, starterCmd := range starterCmds {
-			printConsoleCmd(ctx, stdout, fmt.Sprintf("ns %s", strings.Join(starterCmd.args, " ")))
-			fmt.Fprintf(stdout, "%s\n\n", wordwrap.String(colors.Ctx(ctx).Comment.Apply(starterCmd.description), 80))
-			if err := runCommand(ctx, starterCmd.args); err != nil {
-				return err
-			}
-		}
-
-		// README.md file content and the content to print to console are slightly different.
-		err = generateAndWriteReadmeFile(ctx, stdout)
-		if err != nil {
-			return err
-		}
-		return generateAndPrintReadme(ctx, stdout, dirName)
-	})
-
-	return cmd
+			return generateAndPrintReadme(ctx, stdout, dirName)
+		})
 }
 
 type starterCmd struct {
