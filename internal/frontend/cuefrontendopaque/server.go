ed861bc90e602562224a3c7b705515f2dfa759f8
Nik Zherebtsov <nz@namespacelabs.com>
2022-08-30 16:50:19 +0200

    Opaque servers: parsing service defs, forwarding ports in `ns dev`.

--- internal/frontend/cuefrontendopaque/server.go
+++ internal/frontend/cuefrontendopaque/server.go
@@ -21,11 +21,18 @@ type cueServer struct {
 
 	Args *cuefrontend.ArgsListOrMap `json:"args"`
 	Env  map[string]string          `json:"env"`
+
+	Services map[string]cueService `json:"services"`
 }
 
 type cueIntegration struct {
 	Kind       string `json:"kind"`
 	Dockerfile string `json:"dockerfile"`
+}
+
+type cueService struct {
+	Kind string `json:"kind"`
+	Port int    `json:"port"`
 }
 
 func parseCueServer(ctx context.Context, pl workspace.EarlyPackageLoader, loc workspace.Location, parent, v *fncue.CueV, pp *workspace.Package, opts workspace.LoadPackageOpts) (*schema.Server, *schema.StartupPlan, error) {
@@ -53,6 +60,15 @@ func parseCueServer(ctx context.Context, pl workspace.EarlyPackageLoader, loc wo
 		return nil, nil, fnerrors.UserError(loc, "unsupported integration kind %q", bits.Integration.Kind)
 	}
 
+	for name, svc := range bits.Services {
+		parsed, err := parseService(loc, name, svc)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		out.Service = append(out.Service, parsed)
+	}
+
 	startupPlan := &schema.StartupPlan{
 		Env:  bits.Env,
 		Args: bits.Args.Parsed(),
@@ -60,4 +76,20 @@ func parseCueServer(ctx context.Context, pl workspace.EarlyPackageLoader, loc wo
 
 	server, err := workspace.TransformOpaqueServer(ctx, pl, loc, out, pp, opts)
 	return server, startupPlan, err
+}
+
+func parseService(loc workspace.Location, name string, svc cueService) (*schema.Server_ServiceSpec, error) {
+	if svc.Kind != "http" {
+		return nil, fnerrors.UserError(loc, "service kind is not supported: %s", svc.Kind)
+	}
+
+	parsed := &schema.Server_ServiceSpec{
+		Name: name,
+		Port: &schema.Endpoint_Port{Name: name, ContainerPort: int32(svc.Port)},
+		Metadata: []*schema.ServiceMetadata{{
+			Protocol: svc.Kind,
+		}},
+	}
+
+	return parsed, nil
 }
