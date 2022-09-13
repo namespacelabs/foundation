6e7881df217c8b8f5b76468100f456adb0924bbb
Nik Zherebtsov <nz@namespacelabs.com>
2022-09-13 16:43:07 +0200

    Opaques: Go build integration.

---
+++ integrations/testdata/golang/main.go
@@ -0,0 +1,30 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the EARLY ACCESS SOFTWARE LICENSE AGREEMENT
+// available at http://github.com/namespacelabs/foundation
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"namespacelabs.dev/foundation/std/go/core"
+)
+
+func main() {
+	config, err := core.LoadRuntimeConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	port := config.Current.Port[0].Port
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	log.Printf("Listening on port: %d", port)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
---
+++ integrations/testdata/golang/server.cue
@@ -0,0 +1,21 @@
+server: {
+	name: "myserver"
+
+	integration: go: {}
+
+	env: {
+		NAME: "\($env.name)-Bob"
+	}
+
+	services: {
+		webapi: {
+			port: 4000
+			kind: "http"
+
+			ingress: {
+				internetFacing: true
+				httpRoutes: "*": ["/"]
+			}
+		}
+	}
+}
