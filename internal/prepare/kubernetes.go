0580ce249a63081244cbb87909b1459f830fe36b
Niklas Gehlen <niklas@namespacelabs.com>
2022-09-28 14:39:03 +0200

    nochangelog: unbreak CI during investigation

--- internal/prepare/kubernetes.go
+++ internal/prepare/kubernetes.go
@@ -19,7 +19,8 @@ func PrepareCluster(env planning.Context, confs ...compute.Computable[[]*schema.
 
 	var prepares []compute.Computable[[]*schema.DevHost_ConfigureEnvironment]
 	prepares = append(prepares, confs...)
-	prepares = append(prepares, PrepareOrchestrator(env, kube))
+	// TODO requires a configured registry.
+	// prepares = append(prepares, PrepareOrchestrator(env, kube))
 	prepares = append(prepares, PrepareIngress(env, kube))
 
 	return prepares
