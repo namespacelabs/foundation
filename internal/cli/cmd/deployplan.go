1c181698618c26e5eb782dfe82348b04aea5bdeb
Hugo Santos <hugo@namespacelabs.com>
2022-10-10 13:55:44 +0200

    cmd/deployplan: added ability to deploy from image.

--- internal/cli/cmd/deployplan.go
+++ internal/cli/cmd/deployplan.go
@@ -6,12 +6,18 @@ package cmd
 
 import (
 	"context"
+	"io"
+	"io/fs"
 	"os"
 
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/spf13/cobra"
 	"google.golang.org/protobuf/proto"
+	"namespacelabs.dev/foundation/internal/artifacts/oci"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
+	"namespacelabs.dev/foundation/internal/compute"
 	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/fnfs/tarfs"
 	"namespacelabs.dev/foundation/runtime"
 	"namespacelabs.dev/foundation/schema"
 	"namespacelabs.dev/foundation/std/planning"
@@ -21,9 +27,10 @@ import (
 
 func NewDeployPlanCmd() *cobra.Command {
 	var opts deployOpts
+	var image bool
 
 	cmd := &cobra.Command{
-		Use:    "deploy-plan <path/to/plan>",
+		Use:    "deploy-plan <path/to/plan> | <imageref>",
 		Short:  "Deploys a previously serialized plan.",
 		Args:   cobra.ExactArgs(1),
 		Hidden: true,
@@ -31,6 +38,7 @@ func NewDeployPlanCmd() *cobra.Command {
 
 	cmd.Flags().BoolVar(&opts.alsoWait, "wait", true, "Wait for the deployment after running.")
 	cmd.Flags().StringVar(&opts.outputPath, "output_to", "", "If set, a machine-readable output is emitted after successful deployment.")
+	cmd.Flags().BoolVar(&image, "image", false, "If set to true, the argument represents an image.")
 
 	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
 		root, err := module.FindRoot(ctx, ".")
@@ -38,14 +46,9 @@ func NewDeployPlanCmd() *cobra.Command {
 			return err
 		}
 
-		raw, err := os.ReadFile(args[0])
+		plan, err := loadPlan(ctx, image, args[0])
 		if err != nil {
-			return fnerrors.New("failed to load %q: %w", args[0], err)
-		}
-
-		plan := &schema.DeployPlan{}
-		if err := proto.Unmarshal(raw, plan); err != nil {
-			return fnerrors.New("failed to unmarshal %q: %w", args[0], err)
+			return err
 		}
 
 		config, err := planning.MakeConfigurationCompat(root, root.Workspace(), root.DevHost(), plan.Environment)
@@ -76,3 +79,32 @@ func (se serializedContext) Workspace() planning.Workspace         { return se.r
 func (se serializedContext) Environment() *schema.Environment      { return se.env }
 func (se serializedContext) ErrorLocation() string                 { return se.root.ErrorLocation() }
 func (se serializedContext) Configuration() planning.Configuration { return se.config }
+
+func loadPlan(ctx context.Context, image bool, path string) (*schema.DeployPlan, error) {
+	raw, err := loadPlanContents(ctx, image, path)
+	if err != nil {
+		return nil, fnerrors.New("failed to load %q: %w", path, err)
+	}
+
+	plan := &schema.DeployPlan{}
+	if err := proto.Unmarshal(raw, plan); err != nil {
+		return nil, fnerrors.New("failed to unmarshal %q: %w", path, err)
+	}
+
+	return plan, nil
+}
+
+func loadPlanContents(ctx context.Context, image bool, path string) ([]byte, error) {
+	if image {
+		image, err := compute.GetValue(ctx, oci.ImageP(path, nil, oci.ResolveOpts{}))
+		if err != nil {
+			return nil, err
+		}
+
+		fsys := tarfs.FS{TarStream: func() (io.ReadCloser, error) { return mutate.Extract(image), nil }}
+
+		return fs.ReadFile(fsys, "deployplan.binarypb")
+	}
+
+	return os.ReadFile(path)
+}
