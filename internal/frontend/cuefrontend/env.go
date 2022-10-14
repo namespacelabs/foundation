5c173d95d7b39debd63fbd1cf6b69bdf3cf23d15
Hugo Santos <hugo@namespacelabs.com>
2022-10-14 23:40:33 +0200

    frontend: allow setting `fromSecret` in an environment variable map.

--- internal/frontend/cuefrontend/env.go
+++ internal/frontend/cuefrontend/env.go
@@ -5,29 +5,26 @@
 package cuefrontend
 
 import (
+	"bytes"
 	"encoding/json"
 	"strings"
 
+	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
+	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/protos"
 	"namespacelabs.dev/foundation/schema"
 )
 
 type EnvMap struct {
-	values map[string]string
+	Values map[string]envValue `json:",inline"`
 }
 
-var _ json.Unmarshaler = &EnvMap{}
-
-func (cem *EnvMap) UnmarshalJSON(data []byte) error {
-	var m map[string]string
-
-	if err := json.Unmarshal(data, &m); err != nil {
-		return err
-	}
-
-	cem.values = m
-	return nil
+type envValue struct {
+	v *schema.BinaryConfig_EnvEntry
 }
+
+var _ json.Unmarshaler = &envValue{}
 
 func (cem *EnvMap) Parsed() []*schema.BinaryConfig_EnvEntry {
 	if cem == nil {
@@ -35,11 +32,10 @@ func (cem *EnvMap) Parsed() []*schema.BinaryConfig_EnvEntry {
 	}
 
 	var env []*schema.BinaryConfig_EnvEntry
-	for key, value := range cem.values {
-		env = append(env, &schema.BinaryConfig_EnvEntry{
-			Name:  key,
-			Value: value,
-		})
+	for key, value := range cem.Values {
+		v := protos.Clone(value.v)
+		v.Name = key
+		env = append(env, v)
 	}
 
 	slices.SortFunc(env, func(a, b *schema.BinaryConfig_EnvEntry) bool {
@@ -47,4 +43,40 @@ func (cem *EnvMap) Parsed() []*schema.BinaryConfig_EnvEntry {
 	})
 
 	return env
+}
+
+func (ev *envValue) UnmarshalJSON(data []byte) error {
+	d := json.NewDecoder(bytes.NewReader(data))
+	tok, err := d.Token()
+	if err != nil {
+		return err
+	}
+
+	if tok == json.Delim('{') {
+		var m map[string]string
+		if err := json.Unmarshal(data, &m); err != nil {
+			return err
+		}
+
+		keys := maps.Keys(m)
+		if len(keys) != 1 || keys[0] != "fromSecret" {
+			return fnerrors.BadInputError("when setting an object to a env var map, expected a single key `fromSecret`")
+		}
+
+		// XXX support setting only the secret name.
+		ref, err := schema.ParsePackageRef(m[keys[0]])
+		if err != nil {
+			return err
+		}
+
+		ev.v = &schema.BinaryConfig_EnvEntry{FromSecretRef: ref}
+		return nil
+	}
+
+	if str, ok := tok.(string); ok {
+		ev.v = &schema.BinaryConfig_EnvEntry{Value: str}
+		return nil
+	}
+
+	return fnerrors.BadInputError("failed to parse value, unexpected token %v", tok)
 }
