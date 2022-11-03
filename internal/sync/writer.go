363c9eb3e5e575d267a60a9104688cd350de82cf
Niklas Gehlen <niklas@namespacelabs.com>
2022-11-03 09:35:13 +0100

    nochangelog: no need for named return values

--- internal/sync/writer.go
+++ internal/sync/writer.go
@@ -18,7 +18,7 @@ func SyncWriter(w io.Writer) io.Writer {
 	return &syncWriter{w: w}
 }
 
-func (s *syncWriter) Write(p []byte) (n int, err error) {
+func (s *syncWriter) Write(p []byte) (int, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.w.Write(p)
