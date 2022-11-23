efcd8a908dab33d00671b417eb36040e58dd59db
Ferenc Szalai <szferi@pionlab.com>
2022-11-23 13:17:29 +0100

    adapt test to changes of locationsFromArgs functions

--- internal/cli/fncobra/locbound_test.go
+++ internal/cli/fncobra/locbound_test.go
@@ -14,30 +14,32 @@ import (
 )
 
 func TestPackagesFromArgs(t *testing.T) {
+	opts := ParseLocationsOpts{}
+
 	// Root cwd
-	assertLocationFromArgs(t, ".", "servers/server1", "servers/server1")
-	assertLocationFromArgs(t, ".", "./servers/server1", "servers/server1")
+	assertLocationFromArgs(t, ".", "servers/server1", "servers/server1", opts)
+	assertLocationFromArgs(t, ".", "./servers/server1", "servers/server1", opts)
 
 	// Non-root cwd
-	assertLocationFromArgs(t, "servers", "server1", "servers/server1")
-	assertLocationFromArgs(t, "servers", "./server1", "servers/server1")
-	assertLocationFromArgs(t, "servers", "../extentions/ext1", "extentions/ext1")
-	assertLocationFromArgs(t, "servers/server1", "..", "servers")
+	assertLocationFromArgs(t, "servers", "server1", "servers/server1", opts)
+	assertLocationFromArgs(t, "servers", "./server1", "servers/server1", opts)
+	assertLocationFromArgs(t, "servers", "../extentions/ext1", "extentions/ext1", opts)
+	assertLocationFromArgs(t, "servers/server1", "..", "servers", opts)
 
 	// Fully-qualified package name
-	assertLocationFromArgs(t, ".", "github.com/myuser/mymodule/servers/server1", "servers/server1")
-	assertLocationFromArgs(t, "servers", "github.com/myuser/mymodule/servers/server1", "servers/server1")
-	assertLocationFromArgs(t, "services", "github.com/myuser/mymodule/servers/server1", "servers/server1")
+	assertLocationFromArgs(t, ".", "github.com/myuser/mymodule/servers/server1", "servers/server1", opts)
+	assertLocationFromArgs(t, "servers", "github.com/myuser/mymodule/servers/server1", "servers/server1", opts)
+	assertLocationFromArgs(t, "services", "github.com/myuser/mymodule/servers/server1", "servers/server1", opts)
 	assertLocationFromArgsForModule(t, "services", "namespacelabs.com/myuser2/othermodule/servers/server1",
-		"servers/server1", "namespacelabs.com/myuser2/othermodule")
+		"servers/server1", "namespacelabs.com/myuser2/othermodule", opts)
 
-	assertLocationFromArgs(t, ".", "servers/...", "servers/server1")
+	assertLocationFromArgs(t, ".", "servers/...", "servers/server1", opts)
 
 	// Error cases
-	_, err := locationsFromArgs(context.Background(), moduleName, allModules, "servers", []string{"/abs/path"}, nil)
+	_, _, err := locationsAndPackageRefsFromArgs(context.Background(), moduleName, allModules, "servers", []string{"/abs/path"}, nil, opts)
 	assert.ErrorContains(t, err, "absolute paths are not supported")
 
-	_, err = locationsFromArgs(context.Background(), moduleName, allModules, "servers", []string{"../../othermodule"}, nil)
+	_, _, err = locationsAndPackageRefsFromArgs(context.Background(), moduleName, allModules, "servers", []string{"../../othermodule"}, nil, opts)
 	assert.ErrorContains(t, err, "can't refer to packages outside of the module root")
 }
 
@@ -45,19 +47,19 @@ const moduleName = "github.com/myuser/mymodule"
 
 var allModules = []string{moduleName, "namespacelabs.com/myuser2/othermodule"}
 
-func assertLocationFromArgs(t *testing.T, relCwd string, arg string, expectedRelPath string) {
-	assertLocationFromArgsForModule(t, relCwd, arg, expectedRelPath, moduleName)
+func assertLocationFromArgs(t *testing.T, relCwd string, arg string, expectedRelPath string, opts ParseLocationsOpts) {
+	assertLocationFromArgsForModule(t, relCwd, arg, expectedRelPath, moduleName, opts)
 }
 
-func assertLocationFromArgsForModule(t *testing.T, relCwd string, arg string, expectedRelPath string, expectedModuleName string) {
-	locations, err := locationsFromArgs(context.Background(), moduleName, allModules, relCwd, []string{arg}, func() (parsing.SchemaList, error) {
+func assertLocationFromArgsForModule(t *testing.T, relCwd string, arg string, expectedRelPath string, expectedModuleName string, opts ParseLocationsOpts) {
+	locations, _, err := locationsAndPackageRefsFromArgs(context.Background(), moduleName, allModules, relCwd, []string{arg}, func() (parsing.SchemaList, error) {
 		var p parsing.SchemaList
 		p.Locations = append(p.Locations, fnfs.Location{
 			ModuleName: moduleName,
 			RelPath:    "servers/server1",
 		})
 		return p, nil
-	})
+	}, opts)
 	assert.NilError(t, err)
 	assert.Equal(t, len(locations), 1)
 	assert.Equal(t, locations[0].ModuleName, expectedModuleName)
