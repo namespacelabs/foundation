a1d44258d15e12e44e45e9f62a314c828efbe0ab
Niklas Gehlen <niklas@namespacelabs.com>
2023-01-12 16:48:07 +0100

    nochangelog: prepare release of new orchestrator

--- orchestration/migration.go
+++ orchestration/migration.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Bumping this value leads to an orchestrator upgrade.
-const orchestratorVersion = 12
+const orchestratorVersion = 13
 
 func ExecuteOpts() execution.ExecuteOpts {
 	return execution.ExecuteOpts{
