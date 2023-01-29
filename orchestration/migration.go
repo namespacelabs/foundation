5f9fa84ae70e03ccb5cad5303a286f793df20ac5
Hugo Santos <hugo@namespacelabs.com>
2023-01-30 00:48:56 +0100

    orchestrator: bumped orchestrator version to 19.

--- orchestration/migration.go
+++ orchestration/migration.go
@@ -20,7 +20,7 @@ import (
 )
 
 // Bumping this value leads to an orchestrator upgrade.
-const orchestratorVersion = 18
+const orchestratorVersion = 19
 
 func ExecuteOpts() execution.ExecuteOpts {
 	return execution.ExecuteOpts{
