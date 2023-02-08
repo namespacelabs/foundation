bd6614ddabb4d295828cf8aedfca2d36d8e41eaf
Niklas Gehlen <niklas@namespacelabs.com>
2023-02-08 10:30:58 +0100

    manually wait until deployment is found

--- .github/workflows/scripts/dev.sh
+++ .github/workflows/scripts/dev.sh
@@ -2,11 +2,19 @@
 
 tmux new-session -d -s NsDevSession '/tmp/ns dev --buildkit_import_cache=type=gha --buildkit_export_cache=type=gha,mode=max --golang_use_buildkit=false --naming_no_tls=true internal/testdata/integrations/dockerfile/complex'
 
-tmux ls
+while true ; do
+    RESPONSE=`/tmp/ns kubectl -- rollout status --watch --timeout=90s deployment/mycomplexserver-mdpnh1j3smf8t83e`
 
-sleep 10
+    if [[ $RESPONSE != *"Error from server (NotFound): deployments.apps"* ]]; then
+        break
+    fi
+
+    sleep 5
+done
+
+echo $RESPONSE
+
 /tmp/ns kubectl get all
-/tmp/ns kubectl -- rollout status --watch --timeout=90s deployment/mycomplexserver-mdpnh1j3smf8t83e
 
 tmux send-keys -t NsDevSession -l q
 tmux attach-session -t NsDevSession
