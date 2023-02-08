64584df1bd354744256205302a04daae02d60402
Niklas Gehlen <niklas@namespacelabs.com>
2023-02-08 10:46:49 +0100

    Wait for namespaces and deployments, abort after fixed count.

--- .github/workflows/scripts/dev.sh
+++ .github/workflows/scripts/dev.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-tmux new-session -d -s NsDevSession '/tmp/ns dev --buildkit_import_cache=type=gha --buildkit_export_cache=type=gha,mode=max --golang_use_buildkit=false --naming_no_tls=true internal/testdata/integrations/dockerfile/complex'
+tmux new-session -d -s NsDevSession '/tmp/ns dev --buildkit_import_cache=type=gha --buildkit_export_cache=type=gha,mode=max --golang_use_buildkit=false --naming_no_tls=true internal/testdata/server/gogrpc'
 
+COUNTER=0
 while true ; do
-    RESPONSE=`/tmp/ns kubectl -- rollout status --watch --timeout=90s deployment/mycomplexserver-mdpnh1j3smf8t83e`
+    RESPONSE=`/tmp/ns kubectl -- rollout status --watch --timeout=90s deployment/gogrpcserver-7hzne001dff2rpdxav703bwqwc`
 
-    if [[ $RESPONSE != *"Error from server (NotFound): deployments.apps"* ]]; then
+    if [[ $RESPONSE != *"Error from server (NotFound)"* ]]; then
+        # Don't check what is missing, as we first lack "namespaces" then "deployments.apps"
+        break
+    fi
+
+    let COUNTER++
+
+    if [[ $COUNTER -ge 10 ]]; then
         break
     fi
 
@@ -14,7 +22,7 @@ done
 
 echo $RESPONSE
 
-/tmp/ns kubectl get all
+/tmp/ns kubectl -- get all -A
 
 tmux send-keys -t NsDevSession -l q
 tmux attach-session -t NsDevSession
