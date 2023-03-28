95003e54689943a9175e116a99d8577c6f7e4acc
Ed Ganiukov <ed@namespacelabs.com>
2023-03-28 17:04:50 +0200

    cmd/cluster/build: add support for multi-arch build using buildkit
    Fixes NSL-828

--- internal/cli/cmd/cluster/build.go
+++ internal/cli/cmd/cluster/build.go
@@ -6,6 +6,7 @@ package cluster
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -14,25 +15,44 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/cli/cli/config/configfile"
 	"github.com/docker/cli/cli/config/types"
+	manifeststore "github.com/docker/cli/cli/manifest/store"
+	"github.com/docker/distribution/reference"
 	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	registrytypes "github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/moby/buildkit/client"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
 	buildkitfw "namespacelabs.dev/foundation/framework/build/buildkit"
+	"namespacelabs.dev/foundation/internal/artifacts/oci"
+	"namespacelabs.dev/foundation/internal/artifacts/registry"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
 	"namespacelabs.dev/foundation/internal/files"
 	"namespacelabs.dev/foundation/internal/fnapi"
 	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/localexec"
+	"namespacelabs.dev/foundation/internal/parsing/platform"
+	"namespacelabs.dev/foundation/internal/providers/nscloud"
 	"namespacelabs.dev/foundation/internal/providers/nscloud/api"
 	"namespacelabs.dev/foundation/internal/sdk/buildctl"
 	"namespacelabs.dev/foundation/internal/sdk/host"
 	"namespacelabs.dev/foundation/std/tasks"
+)
+
+const (
+	platformAmd64 = "linux/amd64"
+	platformArm64 = "linux/arm64"
 )
 
 func NewBuildctlCmd() *cobra.Command {
@@ -41,13 +61,14 @@ func NewBuildctlCmd() *cobra.Command {
 		Short: "Run buildctl on the target build cluster.",
 	}
 
+	buildCluster := cmd.Flags().String("cluster", buildCluster, "Set the type of a the build cluster to use.")
 	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
 		buildctlBin, err := buildctl.EnsureSDK(ctx, host.HostPlatform())
 		if err != nil {
 			return fnerrors.New("failed to download buildctl: %w", err)
 		}
 
-		p, err := runBuildProxy(ctx)
+		p, err := runBuildProxy(ctx, *buildCluster)
 		if err != nil {
 			return err
 		}
@@ -81,10 +102,8 @@ type buildProxy struct {
 	Cleanup          func()
 }
 
-func runBuildProxy(ctx context.Context) (*buildProxy, error) {
-	existing := config.LoadDefaultConfigFile(console.Stderr(ctx))
-
-	response, err := api.EnsureBuildCluster(ctx, api.Endpoint, api.EnsureBuildClusterOpts{})
+func runBuildProxy(ctx context.Context, cluster string) (*buildProxy, error) {
+	response, err := api.EnsureBuildCluster(ctx, api.Endpoint, buildClusterOpts(cluster))
 	if err != nil {
 		return nil, err
 	}
@@ -113,6 +132,7 @@ func runBuildProxy(ctx context.Context) (*buildProxy, error) {
 		return nil, err
 	}
 
+	existing := config.LoadDefaultConfigFile(console.Stderr(ctx))
 	// We don't copy over all authentication settings; only some.
 	// XXX replace with custom buildctl invocation that merges auth in-memory.
 	newConfig := configfile.ConfigFile{
@@ -203,111 +223,305 @@ func NewBuildCmd() *cobra.Command {
 
 	dockerFile := cmd.Flags().StringP("file", "f", "", "If set, specifies what Dockerfile to build.")
 	push := cmd.Flags().Bool("push", false, "If specified, pushes the image to the target repository.")
+	dockerLoad := cmd.Flags().Bool("load", false, "If specified, load the image to the local docker registry.")
 	tags := cmd.Flags().StringSliceP("tag", "t", nil, "Attach a tags to the image.")
+	platforms := cmd.Flags().StringSlice("platform", []string{platformAmd64}, "Set target platform for build")
 
 	cmd.RunE = fncobra.RunE(func(ctx context.Context, specifiedArgs []string) error {
+		// XXX: having multiple outputs is not supported by buildctl.
+		if *push && *dockerLoad {
+			return fnerrors.New("usage of both --push and --load flags is not supported")
+		}
 
 		buildctlBin, err := buildctl.EnsureSDK(ctx, host.HostPlatform())
 		if err != nil {
 			return fnerrors.New("failed to download buildctl: %w", err)
 		}
 
-		p, err := runBuildProxy(ctx)
+		clusterProfiles, err := api.GetProfile(ctx, api.Endpoint)
 		if err != nil {
 			return err
 		}
-
-		defer p.Cleanup()
 
 		contextDir := "."
 		if len(specifiedArgs) > 0 {
 			contextDir = specifiedArgs[0]
 		}
 
-		args := []string{
-			"build",
-			"--frontend=dockerfile.v0",
-			"--local", "context=" + contextDir,
-			"--local", "dockerfile=" + contextDir,
-		}
+		reindexer := newImageReindexer(ctx)
+		builtImages := make(map[string][]specs.Platform)
 
-		if *dockerFile != "" {
-			args = append(args, "--opt", "filename="+*dockerFile)
-		}
-
-		var complete func() error
-
-		if *push {
-			imageNames := []string{}
-			for _, tag := range *tags {
-				parsed, err := name.NewTag(tag)
-				if err != nil {
-					return fmt.Errorf("invalid tag %s: %w", tag, err)
-				}
-
-				imageNames = append(imageNames, parsed.Name())
-			}
-
-			args = append(args,
-				// buildctl parses output as csv; need to quote to pass commas to `name`.
-				"--output", fmt.Sprintf("type=image,push=true,%q", "name="+strings.Join(imageNames, ",")),
-			)
-
-			complete = func() error {
-				fmt.Fprintf(console.Stdout(ctx), "Pushed:\n")
-				for _, imageName := range imageNames {
-					fmt.Fprintf(console.Stdout(ctx), "  %s\n", imageName)
-				}
-
-				return nil
-			}
-		} else {
-			f, err := os.CreateTemp("", "docker-image-nsc")
+		errc := make(chan error, len(*platforms))
+		var wg sync.WaitGroup
+		for _, p := range *platforms {
+			platformSpec, err := platform.ParsePlatform(p)
 			if err != nil {
 				return err
 			}
 
-			defer os.Remove(f.Name())
-
-			// We don't actually need it.
-			f.Close()
-
-			imageNames := []string{}
+			var imageNames []string
 			for _, tag := range *tags {
 				parsed, err := name.NewTag(tag)
 				if err != nil {
 					return fmt.Errorf("invalid tag %s: %w", tag, err)
 				}
-				imageNames = append(imageNames, parsed.Name())
+
+				builtImages[parsed.Name()] = append(builtImages[parsed.Name()], platformSpec)
+				imageNames = append(imageNames, imageNameWithPlatform(parsed.Name(), platformSpec))
 			}
 
-			if len(imageNames) > 0 {
-				// buildctl parses output as csv; need to quote to pass commas to `name`.
-				args = append(args, "--output", fmt.Sprintf("type=docker,dest=%s,%q",
-					f.Name(), "name="+strings.Join(imageNames, ",")))
-			} else {
-				args = append(args, "--output", fmt.Sprintf("type=docker,dest=%s",
-					f.Name()))
+			buildProxy, err := runBuildProxy(ctx, resolveBuildCluster(p, clusterProfiles.ClusterPlatform))
+			if err != nil {
+				return err
 			}
+			defer buildProxy.Cleanup()
+			reindexer.setRegistryAccess(buildProxy.RegistryEndpoint, oci.RegistryAccess{Keychain: nscloud.DefaultKeychain})
 
-			complete = func() error {
-				t := time.Now()
-				dockerLoad := exec.CommandContext(ctx, "docker", "load", "-i", f.Name())
-				if err := localexec.RunInteractive(ctx, dockerLoad); err != nil {
+			wg.Add(1)
+			go func(platform string) {
+				defer wg.Done()
+
+				args := []string{
+					"build",
+					"--frontend=dockerfile.v0",
+					"--local", "context=" + contextDir,
+					"--local", "dockerfile=" + contextDir,
+					"--opt", "platform=" + platform,
+				}
+				if *dockerFile != "" {
+					args = append(args, "--opt", "filename="+*dockerFile)
+				}
+
+				var complete func() error
+				switch {
+				case *push:
+					// buildctl parses output as csv; need to quote to pass commas to `name`.
+					args = append(args, "--output",
+						fmt.Sprintf("type=image,push=true,%q", "name="+strings.Join(imageNames, ",")))
+
+					complete = func() error {
+						fmt.Fprintf(console.Stdout(ctx), "Pushed:\n")
+						for _, imageName := range imageNames {
+							fmt.Fprintf(console.Stdout(ctx), "  %s\n", imageName)
+						}
+						return nil
+					}
+				case *dockerLoad:
+					// Load to local Docker registry
+					f, err := os.CreateTemp("", "docker-image-nsc")
+					if err != nil {
+						errc <- err
+						return
+					}
+
+					defer os.Remove(f.Name())
+					// We don't actually need it.
+					f.Close()
+
+					if len(imageNames) > 0 {
+						// buildctl parses output as csv; need to quote to pass commas to `name`.
+						args = append(args, "--output",
+							fmt.Sprintf("type=docker,dest=%s,%q", f.Name(), "name="+strings.Join(imageNames, ",")))
+					} else {
+						args = append(args, "--output", fmt.Sprintf("type=docker,dest=%s", f.Name()))
+					}
+
+					complete = func() error {
+						t := time.Now()
+						dockerLoad := exec.CommandContext(ctx, "docker", "load", "-i", f.Name())
+						if err := localexec.RunInteractive(ctx, dockerLoad); err != nil {
+							return err
+						}
+						fmt.Fprintf(console.Stdout(ctx), "Took %v to upload the image to docker.\n", time.Since(t))
+						return nil
+					}
+				}
+
+				if err := runBuildctl(ctx, buildctlBin, buildProxy, args...); err != nil {
+					errc <- err
+					return
+				}
+
+				if err := complete(); err != nil {
+					errc <- err
+				}
+			}(p)
+		}
+
+		wg.Wait()
+		close(errc)
+
+		var rErr error
+		for err := range errc {
+			if err != nil {
+				rErr = errors.Join(rErr, err)
+			}
+		}
+		if rErr != nil {
+			return rErr
+		}
+
+		for imageName, platformSpecs := range builtImages {
+			fmt.Fprintf(console.Stdout(ctx), "Reindex the image:\n %s\n", imageName)
+
+			switch {
+			case *dockerLoad:
+				if err := reindexer.localReindex(ctx, imageName, platformSpecs); err != nil {
 					return err
 				}
-				took := time.Since(t)
-				fmt.Fprintf(console.Stdout(ctx), "Took %v to upload the image to docker.\n", took)
-				return nil
+			case *push:
+				if err := reindexer.remoteReindex(ctx, imageName, platformSpecs); err != nil {
+					return err
+				}
 			}
 		}
-
-		if err := runBuildctl(ctx, buildctlBin, p, args...); err != nil {
-			return err
-		}
-
-		return complete()
+		return nil
 	})
 
 	return cmd
+}
+
+type imageReindexer struct {
+	manifestStore  manifeststore.Store
+	registryAccess map[string]oci.RegistryAccess
+}
+
+func newImageReindexer(ctx context.Context) *imageReindexer {
+	manifestStore := manifeststore.NewStore(filepath.Join(config.Dir(), "manifests"))
+	return &imageReindexer{
+		manifestStore:  manifestStore,
+		registryAccess: make(map[string]oci.RegistryAccess),
+	}
+}
+
+func (r imageReindexer) localReindex(ctx context.Context, imageName string, platformSpecs []specs.Platform) error {
+	targetRef, err := normalizeReference(imageName)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.manifestStore.GetList(targetRef)
+	if err != nil && !manifeststore.IsNotFound(err) {
+		return err
+	}
+
+	for _, p := range platformSpecs {
+		pNamedRef, err := normalizeReference(imageNameWithPlatform(imageName, p))
+		if err != nil {
+			return err
+		}
+
+		manifests, err := r.manifestStore.Get(targetRef, pNamedRef)
+		if err != nil {
+			return err
+		}
+
+		if err := r.manifestStore.Save(targetRef, pNamedRef, manifests); err != nil {
+			return fnerrors.New("failed to store image manifest: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (r imageReindexer) remoteReindex(ctx context.Context, imageName string, platformSpecs []specs.Platform) error {
+	imageRef, err := name.ParseReference(imageName)
+	if err != nil {
+		return err
+	}
+
+	registryAccess, ok := r.registryAccess[imageRef.Context().RegistryStr()]
+	if !ok {
+		registryAccess = oci.RegistryAccess{Keychain: registry.DefaultDockerKeychain}
+	}
+
+	remoteOpts, err := oci.RemoteOptsWithAuth(ctx, registryAccess, true)
+	if err != nil {
+		return err
+	}
+
+	adds := make([]mutate.IndexAddendum, 0, len(platformSpecs))
+	for _, pSpec := range platformSpecs {
+		imageIndex, err := r.pullImageManifest(ctx, imageNameWithPlatform(imageName, pSpec), pSpec, remoteOpts...)
+		if err != nil {
+			return err
+		}
+
+		if _, err := imageIndex.Digest(); err != nil {
+			return err
+		}
+
+		mediaType, err := imageIndex.MediaType()
+		if err != nil {
+			return err
+		}
+
+		adds = append(adds, mutate.IndexAddendum{
+			Add: imageIndex,
+			Descriptor: v1.Descriptor{
+				MediaType: mediaType,
+				Platform: &v1.Platform{
+					OS:           pSpec.OS,
+					Architecture: pSpec.Architecture,
+					Variant:      pSpec.Variant,
+				},
+			},
+		})
+	}
+
+	imageIndex := mutate.AppendManifests(mutate.IndexMediaType(empty.Index, registrytypes.OCIImageIndex), adds...)
+
+	// The Digest() is requested here to guarantee that the index can indeed be created.
+	if _, err := imageIndex.Digest(); err != nil {
+		return fnerrors.InternalError("failed to compute image index digest: %w", err)
+	}
+
+	return remote.WriteIndex(imageRef, imageIndex, remoteOpts...)
+}
+
+func (r imageReindexer) pullImageManifest(ctx context.Context, ref string, pSpec specs.Platform, opts ...remote.Option) (v1.Image, error) {
+	namedRef, err := name.ParseReference(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	remoteOpts := append(opts, remote.WithPlatform(v1.Platform{
+		OS:           pSpec.OS,
+		Architecture: pSpec.Architecture,
+		Variant:      pSpec.Variant,
+	}))
+	imageManifest, err := remote.Image(namedRef, remoteOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return imageManifest, nil
+}
+
+func (r imageReindexer) setRegistryAccess(registry string, access oci.RegistryAccess) {
+	r.registryAccess[registry] = access
+}
+
+func resolveBuildCluster(platform string, allowedClusterPlatforms []string) string {
+	// If requested platform is arm64 and "arm64" is allowed.
+	if platform == platformArm64 && slices.Contains(allowedClusterPlatforms, "arm64") {
+		return buildClusterArm64
+	}
+
+	return buildCluster
+}
+
+func imageNameWithPlatform(imageName string, platformSpec specs.Platform) string {
+	return fmt.Sprintf("%s-%s", imageName, platformSpec.Architecture)
+}
+
+func normalizeReference(ref string) (reference.Named, error) {
+	namedRef, err := reference.ParseNormalizedNamed(ref)
+	if err != nil {
+		return nil, fnerrors.New("failed to parse image reference: %w", err)
+	}
+	if _, isDigested := namedRef.(reference.Canonical); !isDigested {
+		return reference.TagNameOnly(namedRef), nil
+	}
+	return namedRef, nil
 }
