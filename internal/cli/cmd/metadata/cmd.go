bb379c2dc84fa1cff893d193f10d9e24a8e95fd1
Ed Ganiukov <ed@namespacelabs.com>
2023-04-03 19:16:07 +0200

    cmd/nsc: add command to read VM metadata (#1170)
    Fixes NSL-876

---
+++ internal/cli/cmd/metadata/cmd.go
@@ -0,0 +1,72 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+package metadata
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
+	"namespacelabs.dev/foundation/internal/cli/fncobra"
+	"namespacelabs.dev/foundation/internal/console"
+	"namespacelabs.dev/foundation/internal/fnerrors"
+	"namespacelabs.dev/foundation/internal/providers/nscloud/metadata"
+)
+
+const (
+	defaultMetadataSpecFile = "/var/run/nsc/token.spec"
+)
+
+var supportedMetadataKeys = []string{"id"}
+
+func NewMetadataCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "metadata",
+		Short: "Work with VM metadata.",
+	}
+
+	cmd.AddCommand(newReadCmd())
+
+	return cmd
+}
+
+func newReadCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "read [metadata-key]",
+		Short: "Read a metadata value for the provided key.",
+		Args:  cobra.MinimumNArgs(1),
+	}
+
+	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
+		metadataKey := args[0]
+		if !slices.Contains(supportedMetadataKeys, metadataKey) {
+			return fnerrors.New("reading cluster metadata value for the key %q is not supported", metadataKey)
+		}
+
+		var spec string
+		if s, ok := os.LookupEnv("NSC_TOKEN_SPEC"); ok {
+			spec = s
+		} else {
+			s, err := os.ReadFile(defaultMetadataSpecFile)
+			if err != nil {
+				return fnerrors.New("failed to read metadata spec file: %w", err)
+			}
+
+			spec = string(s)
+		}
+
+		value, err := metadata.FetchValueFromSpec(ctx, spec, metadataKey)
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(console.Stdout(ctx), value+"\n")
+		return nil
+	})
+
+	return cmd
+}
---
+++ internal/providers/nscloud/metadata/metadata.go
@@ -0,0 +1,73 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+package metadata
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"namespacelabs.dev/foundation/internal/console"
+	"namespacelabs.dev/foundation/internal/fnerrors"
+)
+
+const (
+	tokenMetadataKey = "workload_token"
+)
+
+type MetadataSpec struct {
+	Version     string `json:"version,omitempty"`
+	MetadataURL string `json:"metadata_url,omitempty"`
+	TokenURL    string `json:"token_url,omitempty"`
+}
+
+func FetchValueFromSpec(ctx context.Context, specData string, key string) (string, error) {
+	decodedSpec, err := base64.RawStdEncoding.DecodeString(specData)
+	if err != nil {
+		return "", fnerrors.New("metadata spec is invalid")
+	}
+
+	var spec MetadataSpec
+	if err := json.Unmarshal(decodedSpec, &spec); err != nil {
+		fmt.Fprintf(console.Debug(ctx), "failed to unmarshal metadata spec: %v", err)
+		return "", fnerrors.New("metadata spec is invalid")
+	}
+
+	switch spec.Version {
+	case "v1":
+		mdURL := fmt.Sprintf("%s/%s", spec.MetadataURL, key)
+		tCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(tCtx, http.MethodGet, mdURL, nil)
+		if err != nil {
+			return "", fnerrors.New("failed to create metadata request: %w", err)
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return "", fnerrors.New("failed to fetch metadata value: %w", err)
+		}
+
+		defer resp.Body.Close()
+
+		valueBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", fnerrors.New("failed to read metadata value: %w", err)
+		}
+
+		return string(valueBytes), nil
+	default:
+		return "", fnerrors.New("metadata spec is not supported; only support version=v1")
+	}
+}
+
+func FetchTokenFromSpec(ctx context.Context, specData string) (string, error) {
+	return FetchValueFromSpec(ctx, specData, tokenMetadataKey)
+}
