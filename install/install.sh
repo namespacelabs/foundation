dcbda2e8f5e8d145188865cf251465727be038d7
Kirill Nikolaev <kirill@namespacelabs.com>
2023-05-01 09:58:09 +0200

    install.sh: Correctly handle aarch64 architecture (e.g. on ubuntu) and unsupported architectures.

--- install/install.sh
+++ install/install.sh
@@ -67,9 +67,10 @@ do_install() {
 
   echo "Detected ${os} as the host operating system"
 
+  architecture=''
   case $(uname -m) in
     x86_64) architecture="amd64" ;;
-    arm64) architecture="arm64" ;;
+    arm64|aarch64) architecture="arm64" ;;
     arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
   esac
 
