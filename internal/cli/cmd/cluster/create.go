fb69fa07243587f2256a7aa7a266f4e840220c9c
Hugo Santos <hugo@namespacelabs.com>
2023-05-19 21:21:27 +0200

    cluster/create: added --bare and -o json.

--- internal/cli/cmd/cluster/create.go
+++ internal/cli/cmd/cluster/create.go
@@ -31,10 +31,12 @@ func NewCreateCmd(hidden bool) *cobra.Command {
 	ephemeral := cmd.Flags().Bool("ephemeral", true, "Create an ephemeral cluster.")
 	features := cmd.Flags().StringSlice("features", nil, "A set of features to attach to the cluster.")
 	waitKubeSystem := cmd.Flags().Bool("wait_kube_system", false, "If true, wait until kube-system resources (e.g. coredns and local-path-provisioner) are ready.")
+	bare := cmd.Flags().Bool("bare", false, "If set to true, creates an environment with the minimal set of services (e.g. no Kubernetes).")
 
 	outputPath := cmd.Flags().String("output_to", "", "If specified, write the cluster id to this path.")
 	outputJsonPath := cmd.Flags().String("output_json_to", "", "If specified, write cluster metadata as JSON to this path.")
 	outputRegistryPath := cmd.Flags().String("output_registry_to", "", "If specified, write the registry address to this path.")
+	output := cmd.Flags().StringP("output", "o", "plain", "One of plain or json.")
 
 	userSshey := cmd.Flags().String("ssh_key", "", "Injects the specified ssh public key in the created cluster.")
 
@@ -49,6 +51,10 @@ func NewCreateCmd(hidden bool) *cobra.Command {
 			Purpose:       "Manually created from CLI",
 			Features:      *features,
 			InternalExtra: *internalExtra,
+		}
+
+		if *bare {
+			opts.Features = append(opts.Features, "EXP_DISABLE_KUBERNETES")
 		}
 
 		if *userSshey != "" {
@@ -103,19 +109,36 @@ func NewCreateCmd(hidden bool) *cobra.Command {
 			}
 		}
 
-		printNewEnv(ctx, cluster.ClusterId, cluster.Cluster.AppURL)
+		switch *output {
+		case "json":
+			enc := json.NewEncoder(console.Stdout(ctx))
+			enc.SetIndent("", "  ")
 
-		if api.ClusterService(cluster.Cluster, "kubernetes") != nil {
-			stdout := console.Stdout(ctx)
-			style := colors.Ctx(ctx)
-			fmt.Fprintln(stdout)
-			fmt.Fprintf(stdout, "  As a next step, try one of:\n\n")
-			fmt.Fprintf(stdout, "    $ nsc kubectl %s get pod -A\n\n", cluster.ClusterId)
-			fmt.Fprintf(stdout, "    $ nsc kubeconfig write %s\n", cluster.ClusterId)
-			fmt.Fprintf(stdout, "      %s\n", style.Comment.Apply("<follow instructions>"))
-			fmt.Fprintf(stdout, "    $ kubectl get pod -A\n\n")
-			fmt.Fprintf(stdout, "  You can also connect to a shell in the new environment:\n\n")
-			fmt.Fprintf(stdout, "    $ nsc ssh %s\n\n", cluster.ClusterId)
+			// CreateContainersResponse is used here to make sure that the output is compatible with `nsc run`'s.
+			return enc.Encode(api.CreateContainersResponse{
+				ClusterId:  cluster.ClusterId,
+				ClusterUrl: cluster.Cluster.AppURL,
+			})
+
+		default:
+			if *output != "plain" {
+				fmt.Fprintf(console.Warnings(ctx), "defaulting output to plain\n")
+			}
+
+			printNewEnv(ctx, cluster.ClusterId, cluster.Cluster.AppURL)
+
+			if api.ClusterService(cluster.Cluster, "kubernetes") != nil {
+				stdout := console.Stdout(ctx)
+				style := colors.Ctx(ctx)
+				fmt.Fprintln(stdout)
+				fmt.Fprintf(stdout, "  As a next step, try one of:\n\n")
+				fmt.Fprintf(stdout, "    $ nsc kubectl %s get pod -A\n\n", cluster.ClusterId)
+				fmt.Fprintf(stdout, "    $ nsc kubeconfig write %s\n", cluster.ClusterId)
+				fmt.Fprintf(stdout, "      %s\n", style.Comment.Apply("<follow instructions>"))
+				fmt.Fprintf(stdout, "    $ kubectl get pod -A\n\n")
+				fmt.Fprintf(stdout, "  You can also connect to a shell in the new environment:\n\n")
+				fmt.Fprintf(stdout, "    $ nsc ssh %s\n\n", cluster.ClusterId)
+			}
 		}
 
 		return nil
