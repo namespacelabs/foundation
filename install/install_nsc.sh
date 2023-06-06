da2f43d448b1717d246f87ad3f197c0b6510e515
Giulio Micheloni <giulio@namespacelabs.com>
2023-06-06 10:04:18 +0200

    install/install_nsc: fix typo in arm64 arch
    Fixes: PUB-7
    Fixes: #1183

--- install/install_nsc.sh
+++ install/install_nsc.sh
@@ -71,7 +71,7 @@ do_install() {
   architecture=''
   case $(uname -m) in
     x86_64) architecture="amd64" ;;
-    arm6|aarch64) architecture="arm64" ;;
+    arm64|aarch64) architecture="arm64" ;;
     arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
   esac
 
