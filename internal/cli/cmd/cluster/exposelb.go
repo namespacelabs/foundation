48ce4dd004d5a07ab260685e7afca0f46d990311
Niklas Gehlen <niklas@namespacelabs.com>
2023-06-23 18:19:16 +0200

    Implement port and ip selection

--- internal/cli/cmd/cluster/exposelb.go
+++ internal/cli/cmd/cluster/exposelb.go
@@ -31,9 +31,12 @@ func NewExposeLoadBalancerCmd() *cobra.Command {
 	}
 
 	name := cmd.Flags().String("name", "", "If specified, set the name of the exposed ingress. Only permitted when exposing a single port. By default, ingress names are generated.")
-	namespace := cmd.Flags().StringP("namespace", "ns", "default", "Namespace of the service load balancer to export.")
+	namespace := cmd.Flags().String("namespace", "default", "Namespace of the service load balancer to export.")
 	service := cmd.Flags().String("service", "", "Name of the service load balancer to export.")
-	port := cmd.Flags().Int32("service", 0, "Which port of service load balancer to export.")
+	port := cmd.Flags().Int32("port", 0, "Which port of service load balancer to export.")
+
+	ip := cmd.Flags().String("ip", "", "Which ip of service load balancer to export with the ingress.")
+	_ = cmd.Flags().MarkHidden("ip")
 
 	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
 		cluster, _, err := selectRunningCluster(ctx, args)
@@ -45,7 +48,11 @@ func NewExposeLoadBalancerCmd() *cobra.Command {
 			return nil
 		}
 
-		backend, err := selectBackend(ctx, cluster, *namespace, *service, *port)
+		if *service == "" {
+			return fnerrors.New("--service is required")
+		}
+
+		backend, err := selectBackend(ctx, cluster, *namespace, *service, *port, *ip)
 		if err != nil {
 			return err
 		}
@@ -68,7 +75,7 @@ func NewExposeLoadBalancerCmd() *cobra.Command {
 	return cmd
 }
 
-func selectBackend(ctx context.Context, cluster *api.KubernetesCluster, ns, service string, port int32) (*api.IngressBackendEndpoint, error) {
+func selectBackend(ctx context.Context, cluster *api.KubernetesCluster, ns, service string, port int32, ipAddr string) (*api.IngressBackendEndpoint, error) {
 	return tasks.Return(ctx, tasks.Action("nsc.expose-lb").HumanReadablef("Querying exported service load balancers"), func(ctx context.Context) (*api.IngressBackendEndpoint, error) {
 		cfg := clientcmd.NewDefaultClientConfig(ctl.MakeConfig(cluster), nil)
 		restcfg, err := cfg.ClientConfig()
@@ -90,18 +97,71 @@ func selectBackend(ctx context.Context, cluster *api.KubernetesCluster, ns, serv
 			return nil, fnerrors.New("service %q is not a load balancer (found type %q)", service, svc.Spec.Type)
 		}
 
-		var ports []int32
-		for _, port := range svc.Spec.Ports {
-			ports = append(ports, port.Port)
+		port, err := selectPort(svc, port)
+		if err != nil {
+			return nil, err
 		}
 
-		if !slices.Contains(ports, port) {
-			return nil, fnerrors.New("Service %q does not export port %d. Found ports: %v.", service, port, ports)
+		ipAddr, err := selectIpAddr(svc, ipAddr)
+		if err != nil {
+			return nil, err
 		}
 
 		return &api.IngressBackendEndpoint{
-			IpAddr: svc.Spec.ClusterIP, // XXX should this be status.loadbalancer.ingress[0].ip?
+			IpAddr: ipAddr,
 			Port:   port,
 		}, nil
 	})
+}
+
+func selectPort(svc *corev1.Service, port int32) (int32, error) {
+	var ports []int32
+	for _, port := range svc.Spec.Ports {
+		ports = append(ports, port.Port)
+	}
+
+	if port == 0 {
+		switch len(ports) {
+		case 0:
+			return 0, fnerrors.New("service %q exposes no ports", svc.Name)
+		case 1:
+			port = ports[0]
+		default:
+			return 0, fnerrors.New("Service %q exports multiple ports %v. Please select one with --port.", svc.Name, ports)
+		}
+	}
+
+	if !slices.Contains(ports, port) {
+		return 0, fnerrors.New("service %q does not export port %d. Found ports: %v.", svc.Name, port, ports)
+	}
+
+	return port, nil
+}
+
+func selectIpAddr(svc *corev1.Service, ipAddr string) (string, error) {
+	var ipAddrs []string
+	for _, ingress := range svc.Status.LoadBalancer.Ingress {
+		if ingress.IP == "" {
+			continue
+		}
+
+		ipAddrs = append(ipAddrs, ingress.IP)
+	}
+
+	if ipAddr == "" {
+		switch len(ipAddrs) {
+		case 0:
+			return "", fnerrors.New("service %q has no exported ip addresses. This is unexpected. Please contact support@namespacelabs.com.", svc.Name)
+		case 1:
+			return ipAddrs[0], nil
+		default:
+			return "", fnerrors.New("Service %q has multiple exported ip addresses %v. Please select one with --ip.", svc.Name, ipAddrs)
+		}
+	}
+
+	if !slices.Contains(ipAddrs, ipAddr) {
+		return "", fnerrors.New("service %q is not exported on ip address %s. Found addresses: %v.", svc.Name, ipAddr, ipAddrs)
+	}
+
+	return ipAddr, nil
 }
