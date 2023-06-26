d40174bfe8f1baae5b69c0ebf3a5498f54ffc993
Niklas Gehlen <niklas@namespacelabs.com>
2023-06-26 13:48:26 +0200

    regroup commands.

--- internal/cli/cmd/cluster/expose.go
+++ internal/cli/cmd/cluster/expose.go
@@ -26,6 +26,10 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8s "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"namespacelabs.dev/foundation/framework/rpcerrors"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/console"
@@ -33,14 +37,26 @@ import (
 	"namespacelabs.dev/foundation/internal/fnapi"
 	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/providers/nscloud/api"
+	"namespacelabs.dev/foundation/internal/providers/nscloud/ctl"
 	"namespacelabs.dev/foundation/std/tasks"
 )
 
 func NewExposeCmd() *cobra.Command {
+	// `nsc expose` acts as an alias for `nsc expose container`
+	cmd := newExposeContainerCmd("expose [cluster-id]", true)
+
+	cmd.AddCommand(newExposeContainerCmd("container [cluster-id]", false))
+	cmd.AddCommand(newExposeKubernetesCmd())
+
+	return cmd
+}
+
+func newExposeContainerCmd(use string, hidden bool) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "expose [cluster-id]",
-		Short: "Opens a public ingress to the specified exported port.",
-		Args:  cobra.MaximumNArgs(1),
+		Use:    use,
+		Short:  "Opens a public ingress to the specified exported port.",
+		Args:   cobra.MaximumNArgs(1),
+		Hidden: hidden,
 	}
 
 	source := cmd.Flags().String("source", "", "Where to lookup the container.")
@@ -423,4 +439,136 @@ func substr(id string) string {
 	}
 
 	return id
+}
+
+func newExposeKubernetesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "kubernetes [cluster-id]",
+		Short: "Opens a public ingress to the specified Kubernentes service load balancer.",
+		Args:  cobra.MaximumNArgs(1),
+	}
+
+	name := cmd.Flags().String("name", "", "If specified, set the name of the exposed ingress. Only permitted when exposing a single port. By default, ingress names are generated.")
+	namespace := cmd.Flags().String("namespace", "", "Namespace of the service load balancer to expose.")
+	service := cmd.Flags().String("service", "", "Name of the service load balancer to expose.")
+	port := cmd.Flags().Int32("port", 0, "Which exported Load Balancer port to expose.")
+
+	cmd.RunE = fncobra.RunE(func(ctx context.Context, args []string) error {
+		cluster, _, err := selectRunningCluster(ctx, args)
+		if err != nil {
+			return err
+		}
+
+		if cluster == nil {
+			return nil
+		}
+
+		if *service == "" {
+			return fnerrors.New("--service is required")
+		}
+
+		backend, err := selectBackend(ctx, cluster, *namespace, *service, *port)
+		if err != nil {
+			return err
+		}
+
+		resp, err := api.RegisterIngress(ctx, api.Methods, api.RegisterIngressRequest{
+			ClusterId:       cluster.ClusterId,
+			Name:            *name,
+			BackendEndpoint: backend,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(console.Stdout(ctx), "Exported port %d from %s/%s:\n  https://%s\n\n",
+			backend.Port, *namespace, *service, resp.Fqdn)
+
+		return nil
+	})
+
+	return cmd
+}
+
+func selectBackend(ctx context.Context, cluster *api.KubernetesCluster, ns, service string, port int32) (*api.IngressBackendEndpoint, error) {
+	return tasks.Return(ctx, tasks.Action("nsc.expose-lb").HumanReadablef("Querying exported service load balancers"), func(ctx context.Context) (*api.IngressBackendEndpoint, error) {
+		cfg := clientcmd.NewDefaultClientConfig(ctl.MakeConfig(cluster), nil)
+		restcfg, err := cfg.ClientConfig()
+		if err != nil {
+			return nil, fnerrors.New("failed to load kubernetes configuration: %w", err)
+		}
+
+		cli, err := k8s.NewForConfig(restcfg)
+		if err != nil {
+			return nil, fnerrors.New("failed to create kubernetes client: %w", err)
+		}
+
+		svc, err := cli.CoreV1().Services(ns).Get(ctx, service, metav1.GetOptions{})
+		if err != nil {
+			return nil, fnerrors.InvocationError("kubernetes", "failed to query service %q: %w", service, err)
+		}
+
+		if svc.Spec.Type != corev1.ServiceTypeLoadBalancer {
+			return nil, fnerrors.New("service %q is not of type %s (found type %s)", service, corev1.ServiceTypeLoadBalancer, svc.Spec.Type)
+		}
+
+		port, err := selectPort(svc, port)
+		if err != nil {
+			return nil, err
+		}
+
+		ipAddr, err := selectIpAddr(svc)
+		if err != nil {
+			return nil, err
+		}
+
+		return &api.IngressBackendEndpoint{
+			IpAddr: ipAddr,
+			Port:   port,
+		}, nil
+	})
+}
+
+func selectPort(svc *corev1.Service, requestedPort int32) (int32, error) {
+	var ports []int32
+	for _, port := range svc.Spec.Ports {
+		ports = append(ports, port.Port)
+	}
+
+	if requestedPort != 0 {
+		if !slices.Contains(ports, requestedPort) {
+			return 0, fnerrors.New("service %q does not export port %d. Found ports: %v.", svc.Name, requestedPort, ports)
+		}
+
+		return requestedPort, nil
+	}
+
+	switch len(ports) {
+	case 0:
+		return 0, fnerrors.New("service %q exposes no ports", svc.Name)
+	case 1:
+		return ports[0], nil
+	default:
+		return 0, fnerrors.New("Service %q exports multiple ports %v. Please select one with --port.", svc.Name, ports)
+	}
+}
+
+func selectIpAddr(svc *corev1.Service) (string, error) {
+	var ipAddrs []string
+	for _, ingress := range svc.Status.LoadBalancer.Ingress {
+		if ingress.IP == "" {
+			continue
+		}
+
+		ipAddrs = append(ipAddrs, ingress.IP)
+	}
+
+	switch len(ipAddrs) {
+	case 0:
+		return "", fnerrors.New("Service %q has no exported ip addresses. This is unexpected. Please contact support@namespacelabs.com.", svc.Name)
+	case 1:
+		return ipAddrs[0], nil
+	default:
+		return "", fnerrors.New("Service %q has multiple exported ip addresses %v. This is unexpected. Please contact support@namespacelabs.com.", svc.Name, ipAddrs)
+	}
 }
