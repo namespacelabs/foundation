b0766bef3678333be1c0c808c42d5fe0e7e6db98
Niklas Gehlen <niklas@namespacelabs.com>
2023-07-07 17:33:16 +0200

    nsc auth: suggest nscloud runners when GitHub OIDC is unavailable.

--- internal/github/jwt.go
+++ internal/github/jwt.go
@@ -22,7 +22,7 @@ const (
 func JWT(ctx context.Context, audience string) (string, error) {
 	idTokenURL := os.Getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
 	if idTokenURL == "" {
-		return "", fnerrors.UsageError("Please add `id-token: write` to your workflow permissions. See also https://github.com/namespacelabs/nscloud-setup#example", "empty GitHub ID token request URL")
+		return "", fnerrors.UsageError("Please add `id-token: write` to your workflow permissions or migrate to Namespace Runners by adding `runs-on: nscloud`. See also https://github.com/namespacelabs/nscloud-setup#example", "empty GitHub ID token request URL")
 	}
 
 	idToken := os.Getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
