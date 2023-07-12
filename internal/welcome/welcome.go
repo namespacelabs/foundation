5075ea8579cd8230222096cb82917db64ef24dbd
Niklas Gehlen <niklas@namespacelabs.com>
2023-07-12 10:28:47 +0200

    nsc: skip welcome, reduce help message header.
    Fixes NSL-1331

--- internal/welcome/welcome.go
+++ internal/welcome/welcome.go
@@ -39,34 +39,27 @@ const logo = `
 `
 
 func WelcomeMessage(firstRun bool, cmd string) string {
+	if cmd == "nsc" {
+		return `Thank you for trying Namespace Cloud!
+
+Get started at https://cloud.namespace.so/
+`
+	}
+
 	var b strings.Builder
 
 	b.WriteString(logo)
-
-	product := "Namespace"
-	if cmd == "nsc" {
-		product = "Namespace Cloud"
-	}
-
-	b.WriteString(fmt.Sprintf("\n\nThank you for trying %s!\n\n", product))
+	b.WriteString("\n\nThank you for trying Namespace!\n\n")
 
 	if firstRun {
 		// Add this to make clear that this large help won't be printed on every command.
 		b.WriteString(fmt.Sprintf("It appears this is your first run of `%s`. Let us provide some help on how to interact with it:\n", cmd))
 	}
 
-	switch cmd {
-	case "ns":
-		// TODO add more content.
-		b.WriteString("We have assembled some canonical examples at https://namespacelabs.dev/examples.\n")
-		b.WriteString("Our full documentation is located at https://namespace.so/docs.\n")
-
-	case "nsc":
-		b.WriteString("Get started at https://cloud.namespace.so/.\n")
-		// TODO
-	}
-
-	b.WriteString("Tell us what you think on https://community.namespace.so/discord/.\n")
+	b.WriteString(`We have assembled some canonical examples at https://namespacelabs.dev/examples.
+Our full documentation is located at https://namespace.so/docs.
+Tell us what you think on https://community.namespace.so/discord/.
+`)
 
 	if firstRun {
 		b.WriteString(fmt.Sprintf("To see this message again at a later point, just run `%s --help`.\n", cmd))
@@ -76,5 +69,9 @@ func WelcomeMessage(firstRun bool, cmd string) string {
 }
 
 func PrintWelcome(ctx context.Context, firstRun bool, cmd string) {
+	if firstRun && cmd == "nsc" {
+		return
+	}
+
 	fmt.Fprint(console.TypedOutput(ctx, "welcome", console.CatOutputUs), WelcomeMessage(firstRun, cmd))
 }
