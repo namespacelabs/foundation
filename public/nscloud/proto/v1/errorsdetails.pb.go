dd57cc09887bb2863cd72246ff0b07a6a22e20d4
Hugo Santos <hugo@namespacelabs.com>
2023-07-17 23:19:44 +0200

    errordetails: added WorkspaceExpired.

--- public/nscloud/proto/v1/errorsdetails.pb.go
+++ public/nscloud/proto/v1/errorsdetails.pb.go
@@ -165,6 +165,53 @@ func (x *UserMessage) GetMessage() string {
 	return ""
 }
 
+type WorkspaceExpired struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *WorkspaceExpired) Reset() {
+	*x = WorkspaceExpired{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_public_nscloud_proto_v1_errorsdetails_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *WorkspaceExpired) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WorkspaceExpired) ProtoMessage() {}
+
+func (x *WorkspaceExpired) ProtoReflect() protoreflect.Message {
+	mi := &file_public_nscloud_proto_v1_errorsdetails_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WorkspaceExpired.ProtoReflect.Descriptor instead.
+func (*WorkspaceExpired) Descriptor() ([]byte, []int) {
+	return file_public_nscloud_proto_v1_errorsdetails_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *WorkspaceExpired) GetMessage() string {
+	if x != nil {
+		return x.Message
+	}
+	return ""
+}
+
 var File_public_nscloud_proto_v1_errorsdetails_proto protoreflect.FileDescriptor
 
 var file_public_nscloud_proto_v1_errorsdetails_proto_rawDesc = []byte{
@@ -181,11 +228,14 @@ var file_public_nscloud_proto_v1_errorsdetails_proto_rawDesc = []byte{
 	0x01, 0x28, 0x09, 0x52, 0x09, 0x63, 0x6c, 0x75, 0x73, 0x74, 0x65, 0x72, 0x49, 0x64, 0x22, 0x27,
 	0x0a, 0x0b, 0x55, 0x73, 0x65, 0x72, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x18, 0x0a,
 	0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07,
-	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x36, 0x5a, 0x34, 0x6e, 0x61, 0x6d, 0x65, 0x73,
-	0x70, 0x61, 0x63, 0x65, 0x6c, 0x61, 0x62, 0x73, 0x2e, 0x64, 0x65, 0x76, 0x2f, 0x66, 0x6f, 0x75,
-	0x6e, 0x64, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, 0x2f, 0x6e,
-	0x73, 0x63, 0x6c, 0x6f, 0x75, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x76, 0x31, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x2c, 0x0a, 0x10, 0x57, 0x6f, 0x72, 0x6b, 0x73,
+	0x70, 0x61, 0x63, 0x65, 0x45, 0x78, 0x70, 0x69, 0x72, 0x65, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x6d,
+	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65,
+	0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x36, 0x5a, 0x34, 0x6e, 0x61, 0x6d, 0x65, 0x73, 0x70, 0x61,
+	0x63, 0x65, 0x6c, 0x61, 0x62, 0x73, 0x2e, 0x64, 0x65, 0x76, 0x2f, 0x66, 0x6f, 0x75, 0x6e, 0x64,
+	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, 0x2f, 0x6e, 0x73, 0x63,
+	0x6c, 0x6f, 0x75, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -200,11 +250,12 @@ func file_public_nscloud_proto_v1_errorsdetails_proto_rawDescGZIP() []byte {
 	return file_public_nscloud_proto_v1_errorsdetails_proto_rawDescData
 }
 
-var file_public_nscloud_proto_v1_errorsdetails_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_public_nscloud_proto_v1_errorsdetails_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_public_nscloud_proto_v1_errorsdetails_proto_goTypes = []interface{}{
 	(*EnvironmentDestroyed)(nil),   // 0: nsl.clusters.v1.EnvironmentDestroyed
 	(*EnvironmentDoesntExist)(nil), // 1: nsl.clusters.v1.EnvironmentDoesntExist
 	(*UserMessage)(nil),            // 2: nsl.clusters.v1.UserMessage
+	(*WorkspaceExpired)(nil),       // 3: nsl.clusters.v1.WorkspaceExpired
 }
 var file_public_nscloud_proto_v1_errorsdetails_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -256,6 +307,18 @@ func file_public_nscloud_proto_v1_errorsdetails_proto_init() {
 				return nil
 			}
 		}
+		file_public_nscloud_proto_v1_errorsdetails_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*WorkspaceExpired); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -263,7 +326,7 @@ func file_public_nscloud_proto_v1_errorsdetails_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_public_nscloud_proto_v1_errorsdetails_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
