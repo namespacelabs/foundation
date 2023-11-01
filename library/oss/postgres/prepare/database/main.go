107e44b2cb886cc08b75da64f9442caf1751c6df
Hugo Santos <hugo@namespacelabs.com>
2023-11-01 15:09:17 +0100

    Revert "nochangelog: postgres: connect to the database we want to establish."
    This reverts commit b6f12a729154b8fbb2941e8210d83187e99fd377.

--- library/oss/postgres/prepare/database/main.go
+++ library/oss/postgres/prepare/database/main.go
@@ -76,7 +76,7 @@ func main() {
 
 func ensureDatabase(ctx context.Context, cluster *postgresclass.ClusterInstance, name string) (*pgx.Conn, bool, error) {
 	// Postgres needs a database to connect to so we pin one that is guaranteed to exist.
-	postgresConn, err := connect(ctx, cluster, name)
+	postgresConn, err := connect(ctx, cluster, "postgres")
 	if err != nil {
 		return nil, false, err
 	}
