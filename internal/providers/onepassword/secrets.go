d64fe40b8dc4e64f18bb689d4e3e97fa94fe26e3
Niklas Gehlen <niklas@namespacelabs.com>
2023-11-16 11:32:12 +0100

    1Password: provide better error if no account is configured (#1215)
    Towards NSL-2204

--- internal/providers/onepassword/secrets.go
+++ internal/providers/onepassword/secrets.go
@@ -7,7 +7,10 @@ package onepassword
 import (
 	"bytes"
 	"context"
+	"errors"
+	"fmt"
 	"os/exec"
+	"time"
 
 	"namespacelabs.dev/foundation/framework/secrets/combined"
 	"namespacelabs.dev/foundation/internal/console"
@@ -15,19 +18,42 @@ import (
 	"namespacelabs.dev/foundation/universe/onepassword"
 )
 
+const cmdTimeout = time.Minute
+
 func Register() {
 	combined.RegisterSecretsProvider(func(ctx context.Context, cfg *onepassword.Secret) ([]byte, error) {
 		if cfg.SecretReference == "" {
 			return nil, fnerrors.BadInputError("invalid 1Password secret configuration: missing field secret_reference")
 		}
 
-		c := exec.CommandContext(ctx, "op", "read", cfg.SecretReference)
+		readCtx, cancel := context.WithTimeout(ctx, cmdTimeout)
+		defer cancel()
+
+		c := exec.CommandContext(readCtx, "op", "read", cfg.SecretReference)
 
 		var b bytes.Buffer
 		c.Stdout = &b
 		c.Stderr = console.Stderr(ctx)
-		c.Stdin = nil
 		if err := c.Run(); err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				// If no account is configured, `op read` does not fail but waits for user input.
+				// List accounts on timeouts to provide a better error.
+				c := exec.CommandContext(readCtx, "op", "account", "list")
+
+				var b bytes.Buffer
+				c.Stdout = &b
+				c.Stderr = console.Stderr(ctx)
+				if err := c.Run(); err != nil {
+					return nil, fnerrors.InvocationError("1Password", "failed to invoke %q: %w", c.String(), err)
+				}
+
+				if b.String() == "" {
+					return nil, fnerrors.InvocationError("1Password", "no 1Password account configured")
+				}
+
+				fmt.Fprintf(console.Debug(ctx), "Configured 1Password accounts:\n%s\n", b.String())
+			}
+
 			return nil, fnerrors.InvocationError("1Password", "failed to invoke %q: %w", c.String(), err)
 		}
 
