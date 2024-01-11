cf6a3f40a4f8d45d6d01aeb96ac62f507320b962
Niklas Gehlen <niklas@namespacelabs.com>
2024-01-11 11:46:56 +0100

    Redirect pprof debug output to stderr. This allows pprof to be used with `ns t kubectl`.

--- internal/cli/fncobra/main.go
+++ internal/cli/fncobra/main.go
@@ -129,7 +129,7 @@ func doMain(opts MainOpts) (colors.Style, error) {
 		}
 
 		if viper.GetBool("enable_pprof") {
-			go ListenPProf(console.Output(cmd.Context(), "pprof"))
+			go ListenPProf(console.Warnings(cmd.Context()))
 		}
 
 		run = storedrun.New()
