86e20e14270c7bc059c4aa7a1ca708d3b92c13c9
Niklas Gehlen <niklas@namespacelabs.com>
2024-03-28 14:41:24 +0100

    read external secrets (e.g. 1Password) in parallel (#1224)

--- internal/providers/onepassword/secrets.go
+++ internal/providers/onepassword/secrets.go
@@ -23,9 +23,7 @@ import (
 const cmdTimeout = time.Minute
 
 func Register() {
-	p := provider{
-		cache: map[string][]byte{},
-	}
+	p := &provider{}
 
 	combined.RegisterSecretsProvider(func(ctx context.Context, cfg *onepassword.Secret) ([]byte, error) {
 		if cfg.SecretReference == "" {
@@ -37,28 +35,74 @@ func Register() {
 }
 
 type provider struct {
-	mu               sync.Mutex
-	ensureAccountErr error
-	once             sync.Once
-	cache            map[string][]byte
+	once    sync.Once
+	initErr error
 }
 
 func (p *provider) Read(ctx context.Context, ref string) ([]byte, error) {
-	// Serialize reads to ensure there is only one approval pop-up.
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	var data []byte
 
-	if data, ok := p.cache[ref]; ok {
+	p.once.Do(func() {
+		// If no account is configured, `op read` does not fail but waits for user input.
+		// Hence, we ensure on the first read that a user account is indeed configured.
+		if err := ensureAccount(ctx); err != nil {
+			p.initErr = err
+			return
+		}
+
+		// Do the first read serially, so that the user ends up with only one approval popup.
+		res, err := read(ctx, ref)
+		if err != nil {
+			p.initErr = err
+			return
+		}
+
+		data = res
+
+		// XXX hack!
+		// The first read succeeds and unlocks the 1Password client.
+		// Still, the second read can fail with `error initializing client: account is not signed in` if issued too quickly :(
+		time.Sleep(time.Second)
+	})
+
+	// The only writes to p.initErr are inside p.once which is already done at this point.
+	if p.initErr != nil {
+		return nil, p.initErr
+	}
+
+	if data != nil {
+		// First read does not need to repeat.
 		return data, nil
 	}
 
-	return tasks.Return(ctx, tasks.Action("1password.read").Arg("ref", ref), func(ctx context.Context) ([]byte, error) {
-		// If no account is configured, `op read` does not fail but waits for user input.
-		// Hence, we ensure that a user account is indeed configured.
-		if err := p.ensureAccount(ctx); err != nil {
-			return nil, err
+	return read(ctx, ref)
+}
+
+func ensureAccount(ctx context.Context) error {
+	return tasks.Return0(ctx, tasks.Action("1password.ensure"), func(ctx context.Context) error {
+		if os.Getenv("OP_SERVICE_ACCOUNT_TOKEN") != "" {
+			return nil
 		}
 
+		// Handle manual logins.
+		c := exec.CommandContext(ctx, "op", "account", "list")
+
+		var b bytes.Buffer
+		c.Stdout = &b
+		c.Stderr = console.Output(ctx, "1Password")
+		if err := c.Run(); err != nil {
+			return fnerrors.InvocationError("1Password", "failed to invoke %q: %w", c.String(), err)
+		} else if b.String() == "" {
+			return fnerrors.InvocationError("1Password", "no 1Password account configured")
+		}
+
+		fmt.Fprintf(console.Debug(ctx), "Configured 1Password accounts:\n%s\n", b.String())
+		return nil
+	})
+}
+
+func read(ctx context.Context, ref string) ([]byte, error) {
+	return tasks.Return(ctx, tasks.Action("1password.read").Arg("ref", ref), func(ctx context.Context) ([]byte, error) {
 		c := exec.CommandContext(ctx, "op", "read", ref)
 
 		var b bytes.Buffer
@@ -70,33 +114,6 @@ func (p *provider) Read(ctx context.Context, ref string) ([]byte, error) {
 
 		// `\n` is added by `op read`.
 		data := bytes.TrimSuffix(b.Bytes(), []byte{'\n'})
-
-		p.cache[ref] = data
 		return data, nil
 	})
-}
-
-func (p *provider) ensureAccount(ctx context.Context) error {
-	// Only check once if there is an account.
-	p.once.Do(func() {
-		if os.Getenv("OP_SERVICE_ACCOUNT_TOKEN") != "" {
-			return
-		}
-
-		// Handle manual logins.
-		c := exec.CommandContext(ctx, "op", "account", "list")
-
-		var b bytes.Buffer
-		c.Stdout = &b
-		c.Stderr = console.Output(ctx, "1Password")
-		if err := c.Run(); err != nil {
-			p.ensureAccountErr = fnerrors.InvocationError("1Password", "failed to invoke %q: %w", c.String(), err)
-		} else if b.String() == "" {
-			p.ensureAccountErr = fnerrors.InvocationError("1Password", "no 1Password account configured")
-		}
-
-		fmt.Fprintf(console.Debug(ctx), "Configured 1Password accounts:\n%s\n", b.String())
-	})
-
-	return p.ensureAccountErr
 }
