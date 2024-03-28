6c2b4a667188ec9c2b90137b3ddbe3e755a86fbf
Niklas Gehlen <niklas@namespacelabs.com>
2024-03-28 14:31:14 +0100

    dedup parallel secret loading.

--- framework/secrets/combined/combined.go
+++ framework/secrets/combined/combined.go
@@ -37,8 +37,27 @@ type combinedSecrets struct {
 	bindings map[string]*schema.Workspace_SecretBinding
 	local    secrets.SecretsSource
 
-	mu    sync.RWMutex
-	cache map[string][]byte
+	mu      sync.RWMutex
+	loaded  map[string][]byte         // secret ref -> value
+	loading map[string]*loadingSecret // secret ref -> loadingSecret
+}
+
+type resultPair struct {
+	value []byte
+	err   error
+}
+
+type loadingSecret struct {
+	ref  *schema.PackageRef
+	load func(context.Context, *anypb.Any) ([]byte, error)
+	cfg  *anypb.Any
+	cs   *combinedSecrets
+
+	mu      sync.Mutex
+	waiters []chan resultPair
+	waiting int // The first waiter, will also get to the secret load.
+	done    bool
+	result  resultPair
 }
 
 func NewCombinedSecrets(env cfg.Context) (secrets.SecretsSource, error) {
@@ -58,13 +77,14 @@ func NewCombinedSecrets(env cfg.Context) (secrets.SecretsSource, error) {
 	return &combinedSecrets{
 		bindings: bindings,
 		local:    local,
-		cache:    map[string][]byte{},
+		loaded:   map[string][]byte{},
+		loading:  map[string]*loadingSecret{},
 	}, nil
 }
 
 func (cs *combinedSecrets) Load(ctx context.Context, modules pkggraph.ModuleResolver, req *secrets.SecretLoadRequest) (*schema.SecretResult, error) {
 	cs.mu.RLock()
-	value := cs.cache[req.SecretRef.Canonical()]
+	value := cs.loaded[req.SecretRef.Canonical()]
 	cs.mu.RUnlock()
 	if value != nil {
 		return &schema.SecretResult{Value: value, FileContents: &schema.FileContents{Contents: value}}, nil
@@ -76,14 +96,23 @@ func (cs *combinedSecrets) Load(ctx context.Context, modules pkggraph.ModuleReso
 			return nil, fnerrors.BadInputError("%s: no such secrets provider", b.Configuration.TypeUrl)
 		}
 
-		value, err := p(ctx, b.Configuration)
+		cs.mu.Lock()
+		loading := cs.loading[req.SecretRef.Canonical()]
+		if loading == nil {
+			loading = &loadingSecret{
+				ref:  req.SecretRef,
+				load: p,
+				cfg:  b.Configuration,
+				cs:   cs,
+			}
+			cs.loading[req.SecretRef.Canonical()] = loading
+		}
+		cs.mu.Unlock()
+
+		value, err := loading.Get(ctx)
 		if err != nil {
 			return nil, err
 		}
-
-		cs.mu.Lock()
-		cs.cache[req.SecretRef.Canonical()] = value
-		cs.mu.Unlock()
 
 		return &schema.SecretResult{Value: value, FileContents: &schema.FileContents{Contents: value}}, nil
 	}
@@ -93,4 +122,64 @@ func (cs *combinedSecrets) Load(ctx context.Context, modules pkggraph.ModuleReso
 
 func (cs *combinedSecrets) MissingError(missing *schema.PackageRef, missingSpec *schema.SecretSpec, missingServer schema.PackageName) error {
 	return cs.local.MissingError(missing, missingSpec, missingServer)
+}
+
+func (cs *combinedSecrets) complete(ref *schema.PackageRef, res []byte) {
+	cs.mu.Lock()
+	cs.loaded[ref.Canonical()] = res
+	cs.mu.Unlock()
+}
+
+func (l *loadingSecret) Get(ctx context.Context) ([]byte, error) {
+	l.mu.Lock()
+
+	rev := l.waiting
+	l.waiting++
+
+	if rev > 0 {
+		// Someone is already loading the secret.
+		if l.done {
+			defer l.mu.Unlock()
+			return l.result.value, l.result.err
+		}
+
+		// Very important that this is a buffered channel, else the write above will
+		// block forever and deadlock secret loading.
+		ch := make(chan resultPair, 1)
+		l.waiters = append(l.waiters, ch)
+		l.mu.Unlock()
+
+		select {
+		case v, ok := <-ch:
+			if !ok {
+				return nil, fnerrors.InternalError("unexpected eof")
+			}
+			return v.value, v.err
+
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		}
+	}
+
+	l.mu.Unlock()
+	var res resultPair
+	res.value, res.err = l.load(ctx, l.cfg)
+	l.mu.Lock()
+
+	l.done = true
+	l.result = res
+
+	if res.err == nil {
+		l.cs.complete(l.ref, res.value)
+	}
+
+	waiters := l.waiters
+	l.waiters = nil
+	l.mu.Unlock()
+
+	for _, ch := range waiters {
+		ch <- res
+		close(ch)
+	}
+	return res.value, res.err
 }
