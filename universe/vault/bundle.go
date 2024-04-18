690c4e8a47af86aa632285382dfd1cd703c7ad95
Attila Oláh <attilaolah@gmail.com>
2024-04-18 08:21:41 +0200

    vault: add methods for building server/client config.

--- universe/vault/bundle.go
+++ universe/vault/bundle.go
@@ -39,3 +39,28 @@ func (tb TlsBundle) CAPool() *x509.CertPool {
 func (tb TlsBundle) Certificate() (tls.Certificate, error) {
 	return tls.X509KeyPair([]byte(tb.CertificatePem), []byte(tb.PrivateKeyPem))
 }
+
+func (tb TlsBundle) ServerConfig() (*tls.Config, error) {
+	cert, err := tb.Certificate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    tb.CAPool(),
+	}, nil
+}
+
+func (tb TlsBundle) ClientConfid() (*tls.Config, error) {
+	cert, err := tb.Certificate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      tb.CAPool(),
+	}, nil
+}
