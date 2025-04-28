namespacelabs/foundation

commit 81c88bf5821607176a17477cc106a695dcd18eae

buildx: Support connectivity test (#1436)
Adds a connectivity check to `nsc docker buildx status` that actually
prints helpful messages.
The same connectivity check is performed when using remote builders in
ns build(-binary).

While we're here, perform some clean up.

internal/cli/cmd/cluster/server_side_buildx.go
@@ -10,10 +10,12 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
@@ -26,6 +28,11 @@ import (
 	"github.com/natefinch/atomic"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/slices"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"namespacelabs.dev/foundation/internal/fnapi"
 	"namespacelabs.dev/foundation/internal/fnerrors"
 	"namespacelabs.dev/foundation/internal/providers/nscloud/api"
@@ -70,14 +77,94 @@ func PrepareServerSideBuildxProxy(ctx context.Context, stateDir string, platform
 		}
 
 		builderConfigs = append(builderConfigs, BuilderConfig{
-			ServerConfig:   bc,
-			ServerCAPath:   serverCAPath,
-			ClientKeyPath:  privKeyPath,
-			ClientCertPath: clientCertPath,
+			Platform:             bc.GetShape().GetMachineArch() + "/" + bc.GetShape().GetOs(),
+			Arch:                 bc.GetShape().GetMachineArch(),
+			FullBuildkitEndpoint: bc.GetFullBuildkitEndpoint(),
+			ServerCAPath:         serverCAPath,
+			ClientKeyPath:        privKeyPath,
+			ClientCertPath:       clientCertPath,
 		})
 	}
 
 	return builderConfigs, nil
+}
+
+func TestServerSideBuildxProxyConnectivity(ctx context.Context, bc BuilderConfig) (bool, error) {
+	clientConn, err := CreateGrpcClientConn(bc)
+	if err != nil {
+		return false, err
+	}
+
+	defer clientConn.Close()
+
+	streamDesc := &grpc.StreamDesc{
+		ServerStreams: true,
+		ClientStreams: true,
+	}
+
+	clientStream, err := grpc.NewClientStream(ctx, streamDesc, clientConn, "/nsl.buildkit.ConnectivityTest/Ping")
+	if err != nil {
+		return false, fmt.Errorf("failed to create client stream to '%s': %v", bc.FullBuildkitEndpoint, err)
+	}
+
+	empty := &emptypb.Empty{}
+	if err := clientStream.SendMsg(empty); err != nil {
+		return false, fmt.Errorf("failed to send message to '%s': %v", bc.FullBuildkitEndpoint, err)
+	}
+
+	clientStream.CloseSend()
+
+	if err := clientStream.RecvMsg(empty); err != nil {
+		if status.Code(err) == codes.Unimplemented {
+			// The endpoint does not implement nsl.buildkit.ConnectivityTest, but is reachable as a grpc server.
+			return false, nil
+		}
+
+		return false, fmt.Errorf("failed to recv message from '%s': %v", bc.FullBuildkitEndpoint, err)
+	}
+
+	return true, nil
+}
+
+func CreateGrpcClientConn(bc BuilderConfig) (*grpc.ClientConn, error) {
+	endpoint := bc.FullBuildkitEndpoint
+	// buildkit expects this to be a URL
+	parsed, err := url.Parse(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse '%s' as url: %v", endpoint, err)
+	}
+
+	if parsed.Scheme != "tcp" {
+		return nil, fmt.Errorf("don't support scheme '%s' in '%s' yet", parsed.Scheme, endpoint)
+	}
+
+	cfg := &tls.Config{
+		ServerName: parsed.Hostname(),
+		RootCAs:    x509.NewCertPool(),
+	}
+
+	serverCAs, err := os.ReadFile(bc.ServerCAPath)
+	if err != nil {
+		return nil, fmt.Errorf("can't read server CA file '%s': %v", bc.ServerCAPath, err)
+	}
+
+	if ok := cfg.RootCAs.AppendCertsFromPEM(serverCAs); !ok {
+		return nil, fmt.Errorf("can't append server CA certs from '%s'", bc.ServerCAPath)
+	}
+
+	clientCert, err := tls.LoadX509KeyPair(bc.ClientCertPath, bc.ClientKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("can't read certificate/key ('%s'/'%s'): %v", bc.ClientCertPath, bc.ClientKeyPath, err)
+	}
+
+	cfg.Certificates = append(cfg.Certificates, clientCert)
+
+	cl, err := grpc.NewClient(parsed.Host, grpc.WithAuthority(parsed.Hostname()), grpc.WithTransportCredentials(credentials.NewTLS(cfg)))
+	if err != nil {
+		return nil, fmt.Errorf("can't create client to '%s': %v", endpoint, err)
+	}
+
+	return cl, nil
 }
 
 func setupServerSideBuildxProxy(ctx context.Context, stateDir, builderName string, use, defaultLoad bool, dockerCli *command.DockerCli, platforms []api.BuildPlatform, createAtStartup bool, builderTag string) error {
@@ -215,10 +302,12 @@ func fetchClientCert(ctx context.Context, pubKeyPem string) (string, error) {
 }
 
 type BuilderConfig struct {
-	ServerConfig   *builderv1beta.GetBuilderConfigurationResponse
-	ServerCAPath   string
-	ClientKeyPath  string
-	ClientCertPath string
+	Platform             string // e.g. "linux/amd64"
+	Arch                 string // e.g. "amd64"
+	FullBuildkitEndpoint string
+	ServerCAPath         string
+	ClientKeyPath        string
+	ClientCertPath       string
 }
 
 func genPrivAndPublicKeysPEM() ([]byte, []byte, error) {
@@ -304,12 +393,7 @@ func wireRemoteBuildxProxy(dockerCli *command.DockerCli, name string, use, defau
 		}
 
 		for _, bc := range builderConfigs {
-			var platforms []string
-			if bc.ServerConfig.GetShape().GetMachineArch() == "arm64" {
-				platforms = []string{"linux/arm64"}
-			} else {
-				platforms = []string{"linux/amd64"}
-			}
+			platforms := []string{bc.Platform}
 
 			driverOpts := map[string]string{
 				"cert":   bc.ClientCertPath,
@@ -322,7 +406,7 @@ func wireRemoteBuildxProxy(dockerCli *command.DockerCli, name string, use, defau
 				driverOpts["default-load"] = "true"
 			}
 
-			if err := ng.Update(bc.ServerConfig.GetShape().GetMachineArch(), getEndpoint(bc.ServerConfig), platforms, true, true, nil, "", driverOpts); err != nil {
+			if err := ng.Update(bc.Arch, bc.FullBuildkitEndpoint, platforms, true, true, nil, "", driverOpts); err != nil {
 				return err
 			}
 		}
@@ -344,12 +428,4 @@ func wireRemoteBuildxProxy(dockerCli *command.DockerCli, name string, use, defau
 
 		return nil
 	})
-}
-
-func getEndpoint(builderConfig *builderv1beta.GetBuilderConfigurationResponse) string {
-	if builderConfig.GetFullBuildkitEndpoint() != "" {
-		return builderConfig.GetFullBuildkitEndpoint()
-	}
-
-	return "tcp://" + builderConfig.GetBuildkitEndpoint() + ":443"
 }

internal/build/buildkit/client.go
@@ -197,7 +197,17 @@ func (c *clientInstance) Compute(ctx context.Context, _ compute.Resolved) (*Gate
 				return nil, fnerrors.InternalError("expected one builder config, got %d", len(builderConfigs))
 			}
 
-			fmt.Fprintf(console.Info(ctx), "buildkit: using server-side build proxy (endpoint: %s)\n", builderConfigs[0].ServerConfig.FullBuildkitEndpoint)
+			endpoint := builderConfigs[0].FullBuildkitEndpoint
+			fmt.Fprintf(console.Info(ctx), "buildkit: using server-side build proxy (endpoint: %s)\n", endpoint)
+
+			isServerSideProxy, err := cluster.TestServerSideBuildxProxyConnectivity(ctx, builderConfigs[0])
+			if err != nil {
+				fmt.Fprintf(console.Warnings(ctx), "buildkit: connectivity check to '%s' failed: %v\n", endpoint, err)
+			}
+
+			if !isServerSideProxy {
+				fmt.Fprintf(console.Warnings(ctx), "buildkit: '%s' has connectivity but doesn't seem to be Namespace Build Ingress\n", endpoint)
+			}
 
 			return useRemoteClusterViaMtls(ctx, builderConfigs[0])
 		}
@@ -294,7 +304,7 @@ func useRemoteClusterViaEndpoint(ctx context.Context, endpoint string) (*Gateway
 
 func useRemoteClusterViaMtls(ctx context.Context, bc cluster.BuilderConfig) (*GatewayClient, error) {
 	return waitAndConnect(ctx, func(ctx context.Context) (*client.Client, error) {
-		return client.New(ctx, bc.ServerConfig.FullBuildkitEndpoint, client.WithCredentials(bc.ClientCertPath, bc.ClientKeyPath), client.WithServerConfig("", bc.ServerCAPath))
+		return client.New(ctx, bc.FullBuildkitEndpoint, client.WithCredentials(bc.ClientCertPath, bc.ClientKeyPath), client.WithServerConfig("", bc.ServerCAPath))
 
 	})
 }

internal/cli/cmd/cluster/buildproxy.go
@@ -34,10 +34,11 @@ import (
 type ProxyStatus string
 
 const (
-	ProxyStatus_Starting   ProxyStatus = "Starting"
-	ProxyStatus_Running    ProxyStatus = "Running"
-	ProxyStatus_Failing    ProxyStatus = "Failing"
-	ProxyStatus_ServerSide ProxyStatus = "ServerSideProxy"
+	ProxyStatus_Starting              ProxyStatus = "Starting"
+	ProxyStatus_Running               ProxyStatus = "Running"
+	ProxyStatus_Failing               ProxyStatus = "Failing"
+	ProxyStatus_ServerSide            ProxyStatus = "ServerSideProxy"
+	ProxyStatus_ServerSideUnreachable ProxyStatus = "ServerSideProxyUnreachable"
 )
 
 const (
@@ -176,13 +177,14 @@ type proxyStatusDesc struct {
 }
 
 type StatusData struct {
-	Platform       string
-	Status         ProxyStatus
-	LastError      string
-	LogPath        string
-	LastInstanceID string
-	LastUpdate     time.Time
-	Requests       int
+	Platform          string
+	IsServerSideProxy bool
+	Status            ProxyStatus
+	LastError         string
+	LogPath           string
+	LastInstanceID    string
+	LastUpdate        time.Time
+	Requests          int
 }
 
 func (p *proxyStatusDesc) setLastInstanceID(builderID string) {

internal/cli/cmd/cluster/buildx.go
@@ -46,6 +46,9 @@ const (
 	BuildkitProxyPath = "buildkit/" + proxyDir
 )
 
+var ErrBuildxNodeGroupNotFound = errors.New("buildx node group not found")
+var ErrIsNotRemoteDriver = errors.New("buildx node group does not have 'remote' driver")
+
 func newSetupBuildxCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "setup",
@@ -503,34 +506,23 @@ type ServerProxyEndpoint struct {
 	Endpoint string
 }
 
-func getServerSideProxyEndpoints(txn *store.Txn, name string) []ServerProxyEndpoint {
-	var endpoints []ServerProxyEndpoint
-
-	ng, err := txn.NodeGroupByName(name)
-	if err != nil {
-		return endpoints
-	}
-
-	if ng.Driver != "remote" {
-		return endpoints
-	}
-
-	for _, node := range ng.Nodes {
-		for _, plat := range node.Platforms {
-			if strings.Contains(node.Endpoint, "namespaceapis.com") {
-				endpoints = append(endpoints, ServerProxyEndpoint{
-					Platform: plat.OS + "/" + plat.Architecture,
-					Endpoint: node.Endpoint,
-				})
-			}
-		}
-	}
-
-	return endpoints
+func isNamespaceBuildkitEndpoint(endpoint string) bool {
+	return strings.Contains(endpoint, "namespaceapis.com")
 }
 
 func isServerSideProxy(txn *store.Txn, name string) bool {
-	return len(getServerSideProxyEndpoints(txn, name)) != 0
+	configs, err := readRemoteBuilderConfigsTxn(txn, name)
+	if err != nil {
+		return false
+	}
+
+	for _, cfg := range configs {
+		if isNamespaceBuildkitEndpoint(cfg.FullBuildkitEndpoint) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func doClientSideProxyCleanup(ctx context.Context, state string, txn *store.Txn) error {
@@ -731,7 +723,7 @@ func newStatusBuildxCommand() *cobra.Command {
 			return err
 		}
 
-		sspDescs, err := determineServerSideProxyConfig(dockerCli, *name)
+		sspDescs, err := determineServerSideProxyStatus(ctx, dockerCli, *name)
 		if err != nil {
 			return err
 		}
@@ -765,12 +757,20 @@ func newStatusBuildxCommand() *cobra.Command {
 			for _, desc := range descs {
 				fmt.Fprintf(stdout, "Platform: %s\n", desc.Platform)
 				fmt.Fprintf(stdout, "  Status: %s\n", desc.Status)
-				fmt.Fprintf(stdout, "  Last Instance ID: %s\n", desc.LastInstanceID)
-				fmt.Fprintf(stdout, "  Last Update: %v\n", desc.LastUpdate)
-				fmt.Fprintf(stdout, "  Last Error: %v\n", desc.LastError)
-				fmt.Fprintf(stdout, "  Requests Handled: %v\n", desc.Requests)
-				fmt.Fprintf(stdout, "  Log Path: %v\n", desc.LogPath)
-				fmt.Fprintf(stdout, "\n")
+				if desc.IsServerSideProxy {
+					if desc.LastError == "" {
+						fmt.Fprintf(stdout, "  Connectivity check successful\n")
+					} else {
+						fmt.Fprintf(stdout, "  Connectivity error: %s\n", desc.LastError)
+					}
+				} else {
+					fmt.Fprintf(stdout, "  Last Instance ID: %s\n", desc.LastInstanceID)
+					fmt.Fprintf(stdout, "  Last Error: %v\n", desc.LastError)
+					fmt.Fprintf(stdout, "  Last Update: %v\n", desc.LastUpdate)
+					fmt.Fprintf(stdout, "  Requests Handled: %v\n", desc.Requests)
+					fmt.Fprintf(stdout, "  Log Path: %v\n", desc.LogPath)
+					fmt.Fprintf(stdout, "\n")
+				}
 			}
 		}
 
@@ -780,20 +780,89 @@ func newStatusBuildxCommand() *cobra.Command {
 	return cmd
 }
 
-func determineServerSideProxyConfig(dockerCli *command.DockerCli, name string) ([]StatusData, error) {
-	descs := []StatusData{}
+func readRemoteBuilderConfigs(dockerCli *command.DockerCli, name string) ([]BuilderConfig, error) {
+	var res []BuilderConfig
 
 	err := withStore(dockerCli, func(txn *store.Txn) error {
-		endpoints := getServerSideProxyEndpoints(txn, name)
-		for _, e := range endpoints {
-			descs = append(descs, StatusData{
-				Platform: e.Platform,
-				Status:   ProxyStatus_ServerSide,
-			})
+		r, err := readRemoteBuilderConfigsTxn(txn, name)
+		if err != nil {
+			return err
 		}
 
+		res = r
 		return nil
 	})
+
+	return res, err
+}
+
+func readRemoteBuilderConfigsTxn(txn *store.Txn, name string) ([]BuilderConfig, error) {
+	var configs []BuilderConfig
+
+	ng, err := txn.NodeGroupByName(name)
+	if err != nil {
+		if os.IsNotExist(errors.Cause(err)) {
+			return configs, ErrBuildxNodeGroupNotFound
+		}
+
+		return configs, fmt.Errorf("can not query buildx node group '%s': %v", name, err)
+	}
+
+	if ng.Driver != "remote" {
+		return configs, ErrIsNotRemoteDriver
+	}
+
+	for _, node := range ng.Nodes {
+		for _, plat := range node.Platforms {
+			endpoint := BuilderConfig{
+				Platform:             plat.OS + "/" + plat.Architecture,
+				Arch:                 plat.Architecture,
+				FullBuildkitEndpoint: node.Endpoint,
+			}
+
+			if node.DriverOpts != nil {
+				endpoint.ServerCAPath = node.DriverOpts["cacert"]
+				endpoint.ClientCertPath = node.DriverOpts["cert"]
+				endpoint.ClientKeyPath = node.DriverOpts["key"]
+			}
+
+			configs = append(configs, endpoint)
+		}
+	}
+
+	return configs, nil
+}
+
+func determineServerSideProxyStatus(ctx context.Context, dockerCli *command.DockerCli, name string) ([]StatusData, error) {
+	descs := []StatusData{}
+
+	configs, err := readRemoteBuilderConfigs(dockerCli, name)
+	if err != nil {
+		if err == ErrBuildxNodeGroupNotFound || err == ErrIsNotRemoteDriver {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	for _, cfg := range configs {
+		if !isNamespaceBuildkitEndpoint(cfg.FullBuildkitEndpoint) {
+			continue
+		}
+
+		status := StatusData{
+			Platform:          cfg.Platform,
+			IsServerSideProxy: true,
+			Status:            ProxyStatus_ServerSide,
+		}
+
+		if _, err := TestServerSideBuildxProxyConnectivity(ctx, cfg); err != nil {
+			status.Status = ProxyStatus_ServerSideUnreachable
+			status.LastError = err.Error()
+		}
+
+		descs = append(descs, status)
+	}
 
 	return descs, err
 }

commit a86abb2b46eb7a83b9ca9c997ca96a4dd8286e11

buildx: Fix wrong platform specifier
This fixes a regression introduced in 81c88bf5821607176a17477cc106a695dcd18eae

internal/cli/cmd/cluster/server_side_buildx.go
@@ -77,7 +77,7 @@ func PrepareServerSideBuildxProxy(ctx context.Context, stateDir string, platform
 		}
 
 		builderConfigs = append(builderConfigs, BuilderConfig{
-			Platform:             bc.GetShape().GetMachineArch() + "/" + bc.GetShape().GetOs(),
+			Platform:             bc.GetShape().GetOs() + "/" + bc.GetShape().GetMachineArch(),
 			Arch:                 bc.GetShape().GetMachineArch(),
 			FullBuildkitEndpoint: bc.GetFullBuildkitEndpoint(),
 			ServerCAPath:         serverCAPath,