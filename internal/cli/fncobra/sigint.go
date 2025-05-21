1d5b8d6cc6ad02bce2a9abb4529cdb84d3b28f1d
Niklas Gehlen <niklas@namespacelabs.com>
2025-05-21 13:00:02 +0200

    Gracefully exit when running in CI. (#1455)

--- internal/cli/fncobra/sigint.go
+++ internal/cli/fncobra/sigint.go
@@ -11,12 +11,18 @@ import (
 	"time"
 
 	"go.uber.org/atomic"
+	"namespacelabs.dev/foundation/internal/environment"
 )
 
 func WithSigIntCancel(ctx context.Context) (context.Context, func()) {
 	ctx, cancel := context.WithCancel(ctx)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
+
+	cleanupTimeout := 2 * time.Second
+	if environment.IsRunningInCI() {
+		cleanupTimeout = 10 * time.Second
+	}
 
 	go func() {
 		defer signal.Stop(c)
@@ -25,18 +31,19 @@ func WithSigIntCancel(ctx context.Context) (context.Context, func()) {
 		for {
 			select {
 			case <-c:
-				if cancelled.CAS(false, true) {
+				if cancelled.CompareAndSwap(false, true) {
 					cancel()
 
 					// Give cleanups a moment to run, and then force exit.
 					go func() {
-						time.Sleep(2 * time.Second)
+						time.Sleep(cleanupTimeout)
 						os.Exit(2)
 					}()
-				} else {
+				} else if !environment.IsRunningInCI() {
 					// Already cancelled, exit immediately.
 					os.Exit(3)
 				}
+
 			case <-ctx.Done():
 				return
 			}
