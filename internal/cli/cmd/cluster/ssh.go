573c0706645a8e6a97898649a4531972e756aefb
Niklas Gehlen <niklas@namespacelabs.com>
2025-05-28 13:28:27 +0200

    Make wait timeout configurable. (#1467)
    Fixes NSL-5878

--- internal/cli/cmd/cluster/ssh.go
+++ internal/cli/cmd/cluster/ssh.go
@@ -42,6 +42,9 @@ func NewSshCmd() *cobra.Command {
 	forcePty := cmd.Flags().BoolP("force-pty", "t", false, "Force pseudo-terminal allocation.")
 	disablePty := cmd.Flags().BoolP("disable-pty", "T", false, "Disable pseudo-terminal allocation.")
 
+	waitTimeout := cmd.Flags().Duration("wait_timeout", time.Minute, "For how long to wait until the instance becomes ready.")
+	cmd.Flags().MarkHidden("wait_timeout")
+
 	user := cmd.Flags().String("user", "", "The user to connect as.")
 	cmd.Flags().MarkHidden("user")
 
@@ -75,11 +78,15 @@ func NewSshCmd() *cobra.Command {
 		}
 		if *oneshot {
 			opts := api.CreateClusterOpts{
-				KeepAtExit:      false,
-				Purpose:         fmt.Sprintf("Temporary instance for SSH"),
-				WaitClusterOpts: api.WaitClusterOpts{WaitForService: "ssh", WaitKind: "kubernetes"},
-				Duration:        time.Minute,
-				UseComputeAPI:   *computeAPI,
+				KeepAtExit: false,
+				Purpose:    fmt.Sprintf("Temporary instance for SSH"),
+				WaitClusterOpts: api.WaitClusterOpts{
+					WaitForService: "ssh",
+					WaitKind:       "kubernetes",
+					WaitTimeout:    *waitTimeout,
+				},
+				Duration:      time.Minute,
+				UseComputeAPI: *computeAPI,
 			}
 
 			cluster, err := api.CreateAndWaitCluster(ctx, api.Methods, opts)
