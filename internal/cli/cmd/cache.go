d05c63f7f30944c19ad407a5aed4ea883cc4fb4c
Matteo Kamm <matteo@namespacelabs.com>
2025-08-20 18:17:05 +0200

    cache/turborepo: add list teams command (#1537)

--- internal/cli/cmd/cache.go
+++ internal/cli/cmd/cache.go
@@ -6,13 +6,20 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/slices"
 	"namespacelabs.dev/foundation/internal/build/buildkit"
 	"namespacelabs.dev/foundation/internal/cli/fncobra"
 	"namespacelabs.dev/foundation/internal/compute/cache"
+	"namespacelabs.dev/foundation/internal/console"
 	"namespacelabs.dev/foundation/internal/executor"
+	"namespacelabs.dev/foundation/internal/fnapi"
 	"namespacelabs.dev/foundation/std/cfg"
 	"namespacelabs.dev/foundation/std/module"
 )
@@ -24,6 +31,7 @@ func NewCacheCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(newPruneCmd())
+	cmd.AddCommand(newTurborepoCmd())
 
 	return cmd
 }
@@ -67,4 +75,81 @@ func newPruneCmd() *cobra.Command {
 	cmd.Flags().StringArrayVar(&what, "caches", what, "Which caches to prune. List of: foundation, buildkit.")
 
 	return cmd
+}
+
+func newTurborepoCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "turborepo",
+		Short: "Turborepo related operations (e.g. list and expire).",
+	}
+
+	cmd.AddCommand(newTurborepoListTeamsCmd())
+	return cmd
+}
+
+func newTurborepoListTeamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "Lists available teams.",
+		Args:  cobra.NoArgs,
+		RunE: fncobra.RunE(func(ctx context.Context, args []string) error {
+			tokenSrc, err := fnapi.FetchToken(ctx)
+			if err != nil {
+				return err
+			}
+
+			endpoint := turboEndpoint()
+			full := fmt.Sprintf("%s/namespace/teams", endpoint)
+
+			req, err := http.NewRequestWithContext(ctx, "GET", full, nil)
+			if err != nil {
+				return err
+			}
+
+			token, err := tokenSrc.IssueToken(ctx, time.Hour, false)
+			if err != nil {
+				return err
+			}
+
+			req.Header.Add("Authorization", "Bearer "+token)
+			req.Header.Add("User-Agent", "NamespaceCLI/1.0")
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+
+			var teams []string
+			if err := json.NewDecoder(resp.Body).Decode(&teams); err != nil {
+				return fmt.Errorf("decode list teams response: %w", err)
+			}
+
+			if len(teams) == 0 {
+				fmt.Fprintln(console.Info(ctx), "No turborepo teams found")
+				return nil
+			}
+
+			fmt.Fprintln(console.Info(ctx), "Available turborepo teams:")
+			for _, team := range teams {
+				fmt.Fprintln(console.Info(ctx), team)
+			}
+
+			return nil
+		}),
+	}
+
+	return cmd
+}
+
+func turboEndpoint() string {
+	if endpoint := os.Getenv("NSC_TURBO_ENDPOINT"); endpoint != "" {
+		return endpoint
+	}
+
+	return "https://turbo.cache.ord.namespaceapis.com"
 }
