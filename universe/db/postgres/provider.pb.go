9d1b67319b76ac72cdcce82cf756a79902530757
Matteo Kamm <matteo@namespacelabs.com>
2025-08-21 11:26:01 +0200

    db/postgres: ns generate

--- universe/db/postgres/provider.pb.go
+++ universe/db/postgres/provider.pb.go
@@ -11,12 +11,11 @@
 package postgres
 
 import (
-	reflect "reflect"
-	sync "sync"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	durationpb "google.golang.org/protobuf/types/known/durationpb"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
