cf0c4b9faa005b008079e2a71a5e1d7cbbd1cfc8
Matteo Kamm <matteo@namespacelabs.com>
2025-10-23 15:42:02 +0200

    http/panic: add a panic handler (#1591)

---
+++ std/go/http/panic/panic.go
@@ -0,0 +1,27 @@
+// Copyright 2025 Namespace Labs Inc; All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+package panic
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+func PanicIfPanic(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				buf := make([]byte, 1<<20)
+				n := runtime.Stack(buf, true)
+				fmt.Fprintf(os.Stderr, "panic while handling http request: %s %s\n%v\n\n%s", r.Method, r.URL.String(), err, buf[:n])
+				os.Exit(7)
+			}
+		}()
+
+		h.ServeHTTP(w, r)
+	})
+}
