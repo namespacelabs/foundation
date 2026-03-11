21e00f53e368d5e44797d29258c330a2404787de
Niklas Gehlen <niklas@namespacelabs.com>
2026-03-11 13:24:21 +0100

    Align Otel semconv version.

--- std/monitoring/tracing/wire.go
+++ std/monitoring/tracing/wire.go
@@ -24,7 +24,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
 	t "go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/noop"
